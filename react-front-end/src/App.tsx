b529a7cd0b4751c088653dff01f99f77300edb2e
Forrest <light24bulbs@gmail.com>
2021-04-24 15:41:11 -0700

    made requested changes to demo app persistence

--- react-front-end/src/App.tsx
+++ react-front-end/src/App.tsx
@@ -4,37 +4,37 @@ import {SecureForm, SecureInput} from '@esluna/secure-frame-react-sdk';
 import './App.css';
 
 
-interface IProps {
+interface IAppProps {
 }
 
-interface IState {
-    foo?: string
-    bar?: string
+interface IAppState {
+    formState: Record<string, string>
 }
 
 
-class App extends React.Component<IProps, IState> {
+class App extends React.Component<IAppProps, IAppState> {
+
+    // Retrieves tokens from sessionStorage and sets them to component's state
+    constructor(props: IAppProps) {
+        super(props);
+        const dataString = window.sessionStorage.getItem('savedFields');
+        const savedData: Record<string, string> = JSON.parse(dataString || "{}"); // fail through to empty object if nothing set
+        console.log("retrieved Saved Data of ", savedData);
+        this.state = { formState: savedData } ;
+    }
 
     // Saves tokens from form to sessionStorage when form submit called
     saveTokensLocally(formData:Record<string, string>) {
         console.log('submit called, storing tokens', formData);
         window.sessionStorage.setItem('savedFields', JSON.stringify(formData));
     };
-    // Retrieves tokens from sessionStorage and sets them to component's state
-    constructor(props: IProps) {
-        super(props);
-        const dataString = window.sessionStorage.getItem('savedFields');
-        const savedData:IState = JSON.parse(dataString||"{}"); // fail through to empty object if nothing set
-        console.log("retrieved Saved Data of ", savedData);
-        this.state = savedData;
-    }
 
     render() {
       return <div className="App">
           <div className="app-form">
               <SecureForm onSubmit={this.saveTokensLocally}>
-                  <SecureInput name="foo" token={this.state.foo}/>
-                  <SecureInput name="bar" token={this.state.bar}/>
+                  <SecureInput name="foo" token={this.state.formState.foo}/>
+                  <SecureInput name="bar" token={this.state.formState.bar}/>
                   <input type="submit"/>
               </SecureForm>
           </div>
