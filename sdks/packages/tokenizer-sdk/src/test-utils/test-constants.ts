0808a4988040500f87999cbe5bdef35b99f82455
Free Wortley <00free@gmail.com>
2021-05-06 21:29:31 -0500

    Add a test case for the tokenization url download

--- sdks/packages/tokenizer-sdk/src/test-utils/test-constants.ts
+++ sdks/packages/tokenizer-sdk/src/test-utils/test-constants.ts
@@ -1,5 +1,6 @@
 export const TEST_TOKEN = 'eb2985fd-1029-4c68-b93d-79a8827e1e08';
-export const TEST_QUERY_PARAMS = '?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=credential&X-Amz-Date=20';
+export const TEST_S3_FILE_PATH = '9ff28';
+export const TEST_S3_QUERY_PARAMS = '?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=credential&X-Amz-Date=20';
 export const TEST_S3_HEADERS = {
   GET: {
     host: 'localhost',
@@ -17,3 +18,7 @@ export const TEST_S3_HEADERS = {
 export const TEST_TOKENIZER_SECRET = 'a-super-secret-test-secret';
 export const TEST_PLAINTEXT_VALUE = 'some-secret-plaintext-value';
 export const TEST_METADATA = '{"foo-key":true}';
+
+export function makeS3Url(port: number) {
+  return `http://localhost:${port}/${TEST_S3_FILE_PATH}${TEST_S3_QUERY_PARAMS}`;
+}
