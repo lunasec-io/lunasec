8479dd8fcdf1a4d8b60471f1784b9c0c562b0060
breadchris <chris@refinerylabs.io>
2021-05-11 00:57:57 -0400

    add s3 bucket cors

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,6 +2,7 @@
 
 SETUP_ID=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 10)
 CIPHERTEXT_BUCKET_ARN=$(aws s3 mb "s3://ciphertextbucket-$SETUP_ID" | jq .ARN)
+aws s3api put-bucket-cors --bucket $CIPHERTEXT_BUCKET_ARN --cors-configuration "$(cat s3bucket_cors.json)"
 
 SECURE_FRAME_KEYSET_ARN=$(aws secretsmanager create-secret --name "secure-frame-keyset-$SETUP_ID" --secret-binary "$(cat test_secrets/secure_frame_keyset)" | jq .ARN)
 SECURE_FRAME_SIGNING_KEYS_ARN=$(aws secretsmanager create-secret --name "secure-frame-signing-keys-$SETUP_ID" --secret-binary "$(cat test_secrets/secure_frame_signing_keys)}" | jq .ARN)
