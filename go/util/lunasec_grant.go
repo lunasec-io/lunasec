0819a3db33339989ab541958fd951dfac580bcfe
Free Wortley <00free@gmail.com>
2021-06-18 12:27:20 -0500

    Make regex be for an actual grant

--- go/util/lunasec_grant.go
+++ go/util/lunasec_grant.go
@@ -5,7 +5,9 @@ import (
 	"regexp"
 )
 
-var lunasecGrantRegex = regexp.MustCompile("^lunasec-grant-(authentication|detokenization)_((?:[A-Za-z\\d+/]{4})*(?:[A-Za-z\\d+/]{3}=|[A-Za-z\\d+/]{2}==))?$")
+var base64RegexPattern = "(?:[A-Za-z\\d+/]{4})*(?:[A-Za-z\\d+/]{3}=|[A-Za-z\\d+/]{2}==)"
+
+var lunasecGrantRegex = regexp.MustCompile("^lunasec-grant-(authentication|detokenization)_(" + base64RegexPattern + "\\." + base64RegexPattern + "\\." + base64RegexPattern + ")$")
 
 func ParseLunaSecGrantToGrantTypeAndJwt(s string) (string, string, error) {
 	if !lunasecGrantRegex.MatchString(s) {
--- js/sdks/packages/node-sdk/src/index.ts
+++ js/sdks/packages/node-sdk/src/index.ts
@@ -2,5 +2,3 @@ export * from './types/config';
 export * from './secure-resolver/index';
 export * from './token-auth-service/index';
 export * from './express-auth-plugin/index';
-export { decodeBase58 } from './decodeBase58';
-export { encodeBase58 } from './decodeBase58';
