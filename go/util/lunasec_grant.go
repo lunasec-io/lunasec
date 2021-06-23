d5c28c88f1756a8cf330b4b24e794366d2f1ee54
Free Wortley <00free@gmail.com>
2021-06-23 15:06:24 -0500

    Updated grant format

--- go/util/lunasec_grant.go
+++ go/util/lunasec_grant.go
@@ -7,7 +7,7 @@ import (
 
 var base64RegexPattern = "(?:[A-Za-z\\d+/]{4})*(?:[A-Za-z\\d+/]{3}=|[A-Za-z\\d+/]{2}==)"
 
-var lunasecGrantRegex = regexp.MustCompile("^lunasec-grant-(authentication|detokenization)_(" + base64RegexPattern + "\\." + base64RegexPattern + "\\." + base64RegexPattern + ")$")
+var lunasecGrantRegex = regexp.MustCompile("^lunasec:grant:(authentication|detokenization):(" + base64RegexPattern + "\\." + base64RegexPattern + "\\." + base64RegexPattern + ")$")
 
 func ParseLunaSecGrantToGrantTypeAndJwt(s string) (string, string, error) {
 	if !lunasecGrantRegex.MatchString(s) {
