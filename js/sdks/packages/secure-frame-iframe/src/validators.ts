10401ccd3c1194f508cbd0bd7efe82de433a1d70
Forrest <forrest@refinery.io>
2021-07-01 20:30:00 -0700

    made requested changes but types broken
    Former-commit-id: 6afec829b7c0daf9e6eef9e7d2be18774f5d8ba3

--- js/sdks/packages/secure-frame-iframe/src/validators.ts
+++ js/sdks/packages/secure-frame-iframe/src/validators.ts
@@ -14,6 +14,7 @@ export function validate(validatorName: ValidatorName, value: string): boolean {
       // @ts-ignore
       return isSSN(value) || validator.isTaxID(value);
   }
+  throw new Error(`Failed to find a validator of the supplied name`);
 }
 
 function isSSN(value: string) {
