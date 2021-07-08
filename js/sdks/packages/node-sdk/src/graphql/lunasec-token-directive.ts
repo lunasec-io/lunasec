0b7fbe17d53bcde2adb53a5ee3486072883edc9e
Forrest <forrest@refinery.io>
2021-07-08 16:51:26 -0700

    realized selection sets and fields arent the same

--- js/sdks/packages/node-sdk/src/graphql/lunasec-token-directive.ts
+++ js/sdks/packages/node-sdk/src/graphql/lunasec-token-directive.ts
@@ -1,6 +1,6 @@
 import { isToken } from '@lunasec/tokenizer-sdk';
 import { SchemaDirectiveVisitor } from 'apollo-server-express';
-import { defaultFieldResolver, GraphQLField, GraphQLInputField, GraphQLInputObjectType } from 'graphql';
+import { defaultFieldResolver, GraphQLField, GraphQLInputField } from 'graphql';
 
 // TODO: go get a real grant
 // Note we can just throw on any issues, apollo seems to return it all cleanly back to the client
@@ -25,10 +25,10 @@ export class TokenDirective extends SchemaDirectiveVisitor {
     };
   }
   visitInputFieldDefinition(field: GraphQLInputField) {
-    console.log('visitInputFieldDefinition:', field);
+    console.log("'VISITED INPUT FIELD!!!!!!!!!!!!!!:", field);
     field.extensions = { hi: 'there' }; // I was hoping we would be able to read these from the plugin to know that the directive was set but it doesn't show up
   }
-  visitInputObject(object: GraphQLInputObjectType) {
-    console.log('visited object:', object);
-  }
+  // visitInputObject(object: GraphQLInputObjectType) {
+  //   console.log('VISITED INPUT OBJECT!!!!!!!!!!!!!!:', object);
+  // }
 }
