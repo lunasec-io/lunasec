d94fd98a7200815629c8e4776a0a488361e065ac
Forrest <forrest@refinery.io>
2021-07-22 19:00:04 -0700

    OMFG INPUT DIRECTIVES ARE WORKING
    Former-commit-id: 91f1af10e09976730a0bcdaacdeae362c2dba2dc
    Former-commit-id: c09343a28b62d0fde3dc7dd62acede5263e4298f

--- js/sdks/packages/node-sdk/src/grant-service/index.ts
+++ js/sdks/packages/node-sdk/src/grant-service/index.ts
@@ -13,39 +13,73 @@ export class LunaSecGrantService {
     this.sessionIdProvider = sessionIdProvider;
   }
 
-  public async create(sessionId: string, tokenId: string) {
-    if (!isToken(tokenId)) {
-      throw new Error('Attempted to create a LunaSec Token Grant from a string that didnt look like a token');
-    }
+  private async initializeTokenizer() {
     // TODO (cthompson) as long as the node-sdk is the source of truth for authentication
     // this is ok. Once we are using an auth provider for this information, this will need to change.
     // in the future this will happen inside a lambda instead of making a request to the go server
+    const authenticationToken = await this.auth.createAuthenticationJWT({});
+    return new Tokenizer({
+      authenticationToken: authenticationToken.toString(),
+    });
+  }
+
+  // This private function handles the creating of just one grant, and is used by the public function below
+  private async createOneGrant(sessionId: string, token: string) {
+    if (!isToken(token)) {
+      throw new Error('Attempted to create a LunaSec Token Grant from a string that didnt look like a token');
+    }
+    const tokenizer = await this.initializeTokenizer();
+    const resp = await tokenizer.setGrant(sessionId, token, 'read_token');
+    if (!resp.success) {
+      throw new Error(`unable to set detokenization grant for: ${token}`);
+    }
+  }
+
+  // Public create can also handle arrays for people's convenience, so it mostly deals with handling the array and passes the verifying logic to the private function above
+  public async create(sessionId: string, tokenOrTokens: string | string[]) {
+    if (Array.isArray(tokenOrTokens)) {
+      const grantPromises: Promise<void>[] = [];
+      tokenOrTokens.forEach((t) => {
+        grantPromises.push(this.createOneGrant(sessionId, t));
+      });
+      return await Promise.all(grantPromises);
+    } else {
+      return await this.createOneGrant(sessionId, tokenOrTokens);
+    }
+  }
+
+  // This private function handles the verifying of just one grant, and is used by the public function below
+  private async verifyOneGrant(sessionId: string, tokenId: string, grantType: GrantType) {
     const authenticationToken = await this.auth.createAuthenticationJWT({});
 
     const tokenizer = new Tokenizer({
       authenticationToken: authenticationToken.toString(),
     });
-    const resp = await tokenizer.setGrant(sessionId, tokenId, 'read_token');
-    if (!resp.success) {
-      throw new Error(`unable to set detokenization grant for: ${tokenId}`);
-    }
-  }
 
-  public async verify(sessionId: string, tokenId: string, grantType: GrantType) {
     if (tokenId === '') {
       return Promise.resolve(); // no point in verifying empty tokens, allow them to be written to the db
     }
     if (!isToken(tokenId)) {
       throw new Error('Attempted to verify a LunaSec Token Grant from a string that didnt look like a token');
     }
-    const authenticationToken = await this.auth.createAuthenticationJWT({});
 
-    const tokenizer = new Tokenizer({
-      authenticationToken: authenticationToken.toString(),
-    });
     const resp = await tokenizer.verifyGrant(sessionId, tokenId, grantType);
     if (!resp.success) {
       throw new Error(`unable to verify tokenization grant for: ${tokenId}`);
+    }
+  }
+
+  // Public verify can also handle arrays for people's convenience, so it mostly deals with handling the array and passes the verifying logic to the private function above
+  public async verify(sessionId: string, tokenOrTokens: string | string[], grantType: GrantType) {
+    // Todo: dry up this array handling from above, we are doing it twice
+    if (Array.isArray(tokenOrTokens)) {
+      const grantPromises: Promise<void>[] = [];
+      tokenOrTokens.forEach((t) => {
+        grantPromises.push(this.verifyOneGrant(sessionId, t, grantType));
+      });
+      return await Promise.all(grantPromises);
+    } else {
+      return await this.verifyOneGrant(sessionId, tokenOrTokens, grantType);
     }
   }
 
@@ -71,11 +105,11 @@ export class LunaSecGrantService {
     return sessionId;
   }
 
-  public async createWithAutomaticSessionId(req: Request, tokenId: string) {
-    return this.create(await this.getSessionIdFromReq(req), tokenId);
+  public async createWithAutomaticSessionId(req: Request, token: string | string[]) {
+    return this.create(await this.getSessionIdFromReq(req), token);
   }
 
-  public async verifyWithAutomaticSessionId(req: Request, tokenId: string, grantType: GrantType) {
-    return this.verify(await this.getSessionIdFromReq(req), tokenId, grantType);
+  public async verifyWithAutomaticSessionId(req: Request, token: string | string[], grantType: GrantType) {
+    return this.verify(await this.getSessionIdFromReq(req), token, grantType);
   }
 }
