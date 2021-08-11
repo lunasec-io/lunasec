78269b5b5159b346bf73bc9380e02651664902c0
Free Wortley <00free@gmail.com>
2021-08-11 16:18:45 -0500

    More changes
    Former-commit-id: c4612a91c800f02acea5bc653c19277e76ce0f61
    Former-commit-id: ed986ef1e75bebe4dc125125b179cf73670745dd

--- .github/print-docker-logs.sh
+++ .github/print-docker-logs.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-docker ps -a | grep "lunasec-monorepo_customer-front-end" | cut -c1-12
+sleep 2
+
+echo "# Docker Container Info"
+
+docker ps -a
 
 docker logs "$(docker ps -a | grep "lunasec-monorepo_customer-front-end" | cut -c1-12)"
+
+echo "# Current Directory Info"
+
+pwd
+
+ls -lisah
