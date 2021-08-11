d172c20a76345e28903afdc6e10382f48c6cbef5
Free Wortley <00free@gmail.com>
2021-08-11 15:21:45 -0500

    CI attempt

--- .github/print-docker-logs.sh
+++ .github/print-docker-logs.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-docker logs "$(docker ps -a | grep "lunasec-node-monorepo_customer-front-end" | cut -c1-12)"
+docker ps -a | grep "lunasec-monorepo_customer-front-end" | cut -c1-12
+
+docker logs "$(docker ps -a | grep "lunasec-monorepo_customer-front-end" | cut -c1-12)"
