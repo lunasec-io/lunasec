82e558ad6aff2eb1e5ccbfab3e1df8bf600f17f2
breadchris <chris@lunasec.io>
2021-08-19 23:55:09 -0400

    demo app
    Former-commit-id: aa394e29e33c0a4befdacc0c0fd5c6aac5a3255e
    Former-commit-id: 6dd3e12e6838102112deefc4b953a012824e6678

---
+++ js/demo-apps/packages/full-example-apps/src/server/dedicated-tokenizer/passport-auth-example/routes/auth-router.ts
@@ -0,0 +1,84 @@
+import {Router} from 'express';
+import passport from 'passport';
+import crypto from "crypto";
+import {db} from "../db";
+
+export function authRouter() {
+  const router = Router();
+
+  router.post(
+      '/login',
+      (req, res, next) => {
+        passport.authenticate('json',
+  (err, user) => {
+            if (err) {
+              return res.json({
+                success: false,
+                error: err
+              });
+            }
+            req.login(user, function(err) {
+              if (err) {
+                return res.json({
+                  success: false,
+                  error: err
+                });
+              }
+              return res.json({
+                success: true
+              });
+            });
+          })(req, res, next);
+      }
+  );
+
+  router.get('/logout', function (req, res, next) {
+    req.logout();
+    res.redirect('/');
+  });
+
+  router.post('/signup', function(req, res, next) {
+    const salt = crypto.randomBytes(16);
+    crypto.pbkdf2(req.body.password, salt, 10000, 32, 'sha256', function(err, hashedPassword) {
+      if (err) {
+        return res.json({
+          success: false,
+          error: err
+        });
+      }
+
+      db.run('INSERT INTO users (username, hashed_password, salt, name) VALUES (?, ?, ?, ?)', [
+        req.body.username,
+        hashedPassword,
+        salt,
+        req.body.name
+      ], function(err) {
+        if (err) {
+          return res.json({
+            success: false,
+            error: err
+          });
+        }
+
+        var user = {
+          id: this.lastID.toString(),
+          username: req.body.username,
+          displayName: req.body.name
+        };
+        req.login(user, function(err) {
+          if (err) {
+            return res.json({
+              success: false,
+              error: err
+            });
+          }
+          return res.json({
+            success: true
+          });
+        });
+      });
+    });
+  });
+
+  return router;
+}
