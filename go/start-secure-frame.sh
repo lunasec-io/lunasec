a318da73ad20c31c8cee1636983fe7628215d881
Free Wortley <free@lunasec.io>
2021-08-29 15:05:08 -0500

    Commit changes that make everything cleaner

--- go/start-secure-frame.sh
+++ go/start-secure-frame.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# nohup makes it so that the process will return after starting and this lets IntelliJ continue along to then attach to the process.
 nohup env $(cat ../.env.host | xargs) dlv --listen=:2345 --headless=true --api-version=2 --accept-multiclient exec ./build/secureframe_dev &
-sleep 1
+
+# Just in case the process takes a sec to start.
+sleep 0.2
 
