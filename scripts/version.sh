fc27e4bed7172c903051a84607e4fccde4ad4828
breadchris <chris@lunasec.io>
2021-09-07 17:14:33 -0400

    update version script

--- scripts/version.sh
+++ scripts/version.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 lerna version prerelease
-
-VERSION=$(node -p "require(\"./package.json\").version")
-git tag "$VERSION"
-git push origin "$VERSION"
+#
+#VERSION=$(node -p "require(\"./package.json\").version")
+#git tag "$VERSION"
+#git push origin "$VERSION"
 
