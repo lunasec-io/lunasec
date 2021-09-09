9bd1c96a642979215392b27aa882276023854402
Forrest <forrest@lunasec.io>
2021-09-09 14:25:30 -0700

    react demo app cleanup
    Former-commit-id: 3b867d1c89232e76ca3d72a3951052f5da09cb6d
    Former-commit-id: 8ae8d19d1a6f0102d0680b50f41b55e0e8c98b3d

--- js/demo-apps/packages/react-front-end/src/dedicated-tokenizer/passport-auth-example/components/secure-components/SecureParagraphDemo.tsx
+++ js/demo-apps/packages/react-front-end/src/dedicated-tokenizer/passport-auth-example/components/secure-components/SecureParagraphDemo.tsx
@@ -1,5 +1,5 @@
 import { SecureParagraph } from '@lunasec/react-sdk';
-import { Card, CardContent, CardHeader, Grid, makeStyles, Typography } from '@material-ui/core';
+import { Card, CardContent, CardHeader, Grid, Typography } from '@material-ui/core';
 import { Alert, AlertTitle } from '@material-ui/lab';
 import React, { useState } from 'react';
 
@@ -21,29 +21,29 @@ export const SecureParagraphDemo: React.FunctionComponent = () => {
     );
   }
 
-  function renderAlerts() {
-    return (
-      <>
-        {error !== null ? (
-          <Alert
-            onClose={() => {
-              setError(null);
-            }}
-            severity="error"
-          >
-            <AlertTitle>Error</AlertTitle>
-            {error}
-          </Alert>
-        ) : null}
-      </>
-    );
+  function renderErrors() {
+    if (error !== null) {
+      return (
+        <Alert
+          onClose={() => {
+            setError(null);
+          }}
+          severity="error"
+        >
+          <AlertTitle>Error</AlertTitle>
+          {error}
+        </Alert>
+      );
+    }
+    return null;
   }
+
   return (
     <Grid item xs={12}>
       <Card>
         <CardHeader title={`User: ${user.username}`} />
         <CardContent>
-          {renderAlerts()}
+          {renderErrors()}
           <Typography>Social Security Number:</Typography>
           <SecureParagraph token={user.ssn_token} errorHandler={(e) => setError(e.message)} />
         </CardContent>
