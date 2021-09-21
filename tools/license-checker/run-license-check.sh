31ff9422fc76e451e2ef38c4c8b206aef0013728
Free Wortley <free@lunasec.io>
2021-09-20 22:50:25 -0500

    Remove installation step for licensing tool
    Former-commit-id: 1d8e15690bdf79efc67900daa43a4d78e461622f
    Former-commit-id: ffebf56579d6c7cf2fa05d0215f803aded06b6da

--- tools/license-checker/run-license-check.sh
+++ tools/license-checker/run-license-check.sh
@@ -14,11 +14,11 @@ LICENSE_TOOL_PATH="$LICENSE_TOOL_DIRECTORY/skywalking-license-eye-0.1.0-bin/bin/
 # TODO: DRY up the calls to the license tool
 
 if [ "$LICENSE_TOOL_COMMAND" == "fix" ]; then
-  $LICENSE_TOOL_PATH -c $LICENSE_TOOL_DIRECTORY/configs/apache2.yaml -v debug header fix
-  $LICENSE_TOOL_PATH -c $LICENSE_TOOL_DIRECTORY/configs/CC-BY-SA-4_0.yaml -v debug header fix
+  $LICENSE_TOOL_PATH -c $LICENSE_TOOL_DIRECTORY/configs/apache2.yaml -v info header fix
+  $LICENSE_TOOL_PATH -c $LICENSE_TOOL_DIRECTORY/configs/CC-BY-SA-4_0.yaml -v info header fix
 elif [ "$LICENSE_TOOL_COMMAND" == "check" ]; then
-  $LICENSE_TOOL_PATH -c $LICENSE_TOOL_DIRECTORY/configs/apache2.yaml -v debug header check
-  $LICENSE_TOOL_PATH -c $LICENSE_TOOL_DIRECTORY/configs/CC-BY-SA-4_0.yaml -v debug header check
+  $LICENSE_TOOL_PATH -c $LICENSE_TOOL_DIRECTORY/configs/apache2.yaml -v info header check
+  $LICENSE_TOOL_PATH -c $LICENSE_TOOL_DIRECTORY/configs/CC-BY-SA-4_0.yaml -v info header check
 else
     echo "Missing license check command -- Must be 'fix' or 'check'."
 fi
