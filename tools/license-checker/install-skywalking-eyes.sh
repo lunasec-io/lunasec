63823e2dcdbc5bd58fca60413a2383141d506533
Free Wortley <free@lunasec.io>
2021-09-20 23:52:00 -0500

    Compile Skywalking Eyes from source to fix Typescript bug
    Former-commit-id: be21c6e64f7c2a858d4d91b16e3766986359667a
    Former-commit-id: 49b85c5f75326b2aa3677c3b4c07aef5d3ffab1b

--- tools/license-checker/install-skywalking-eyes.sh
+++ tools/license-checker/install-skywalking-eyes.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 echo "Fetching Apache Skywalking Eye source code..."
-#wget https://apache.osuosl.org/skywalking/eyes/0.1.0/skywalking-license-eye-0.1.0-bin.tgz
+git clone https://github.com/freeqaz/skywalking-eyes
 
-git clone https://github.com/apache/skywalking-eyes
 cd skywalking-eyes
 
 echo "Checking out specific release..."
-git checkout 2cfd5a18c0d4b03cb8b69f3412d4d53737d831a6
+git checkout 06b2db34bb45c2c61c6c2c4e0263da9c5db4db79
 
 echo "Building from source..."
 make build
