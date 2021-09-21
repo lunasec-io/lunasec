be21c6e64f7c2a858d4d91b16e3766986359667a
Free Wortley <free@lunasec.io>
2021-09-20 23:52:00 -0500

    Compile Skywalking Eyes from source to fix Typescript bug

--- tools/license-checker/run-license-check.sh
+++ tools/license-checker/run-license-check.sh
@@ -4,19 +4,24 @@
 
 LICENSE_TOOL_DIRECTORY=$(dirname $0)
 
-echo $LICENSE_TOOL_DIRECTORY
+# Set our location to the root of the repo
+cd $LICENSE_TOOL_DIRECTORY || exit
+
+ROOT_REPO_DIRECTORY="$LICENSE_TOOL_DIRECTORY/tools/license-checker"
+
+cd ../../ || exit
 
 LICENSE_TOOL_COMMAND="$1"
-LICENSE_TOOL_PATH="$LICENSE_TOOL_DIRECTORY/skywalking-eyes/bin/linux/license-eye"
+LICENSE_TOOL_PATH="$ROOT_REPO_DIRECTORY/skywalking-eyes/bin/linux/license-eye"
 
 # TODO: DRY up the calls to the license tool
 
 if [ "$LICENSE_TOOL_COMMAND" == "fix" ]; then
-  $LICENSE_TOOL_PATH -c $LICENSE_TOOL_DIRECTORY/configs/apache2.yaml -v info header fix
-  $LICENSE_TOOL_PATH -c $LICENSE_TOOL_DIRECTORY/configs/CC-BY-SA-4_0.yaml -v info header fix
+  $LICENSE_TOOL_PATH -c "$ROOT_REPO_DIRECTORY"/configs/apache2.yaml -v error header fix
+  $LICENSE_TOOL_PATH -c "$ROOT_REPO_DIRECTORY"/configs/CC-BY-SA-4_0.yaml -v error header fix
 elif [ "$LICENSE_TOOL_COMMAND" == "check" ]; then
-  $LICENSE_TOOL_PATH -c $LICENSE_TOOL_DIRECTORY/configs/apache2.yaml -v info header check
-  $LICENSE_TOOL_PATH -c $LICENSE_TOOL_DIRECTORY/configs/CC-BY-SA-4_0.yaml -v info header check
+  $LICENSE_TOOL_PATH -c "$ROOT_REPO_DIRECTORY"/configs/apache2.yaml -v error header check
+  $LICENSE_TOOL_PATH -c "$ROOT_REPO_DIRECTORY"/configs/CC-BY-SA-4_0.yaml -v error header check
 else
     echo "Missing license check command -- Must be 'fix' or 'check'."
 fi
