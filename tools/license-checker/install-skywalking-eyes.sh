e5e28f309b2d055ff39d16dcdf45180cb5263a11
Free Wortley <free@lunasec.io>
2021-09-22 12:52:02 -0500

    Use forked version of Apache Skywalking Eyes to fix TypeScript bug
    Former-commit-id: de67bdd82754ca2571bb5d4c4e93b8332e9e4ac3
    Former-commit-id: fab8b86dfe9c892259ec9bc8c19d802ee936427c

--- tools/license-checker/install-skywalking-eyes.sh
+++ tools/license-checker/install-skywalking-eyes.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+# TODO: Replace the cloned version with the "main" Apache version from here:
+# https://github.com/apache/skywalking-eyes
+# Currently waiting for a new Docker release to be cut with the TypeScript fixes.
+
 echo "Fetching Apache Skywalking Eye source code..."
-git clone https://github.com/freeqaz/skywalking-eyes
+git clone https://github.com/freeqaz/skywalking-eyes --depth 1
 
 cd skywalking-eyes
 
