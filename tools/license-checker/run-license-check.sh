de67bdd82754ca2571bb5d4c4e93b8332e9e4ac3
Free Wortley <free@lunasec.io>
2021-09-22 12:52:02 -0500

    Use forked version of Apache Skywalking Eyes to fix TypeScript bug

--- tools/license-checker/run-license-check.sh
+++ tools/license-checker/run-license-check.sh
@@ -2,17 +2,12 @@
 
 # TODO: Add support for OSes other than Linux.
 
-LICENSE_TOOL_DIRECTORY=$(dirname $0)
-
-# Set our location to the root of the repo
-cd $LICENSE_TOOL_DIRECTORY || exit
-
-ROOT_REPO_DIRECTORY="$LICENSE_TOOL_DIRECTORY/tools/license-checker"
-
-cd ../../ || exit
+ROOT_REPO_DIRECTORY=$(dirname "$0")
 
 LICENSE_TOOL_COMMAND="$1"
 LICENSE_TOOL_PATH="$ROOT_REPO_DIRECTORY/skywalking-eyes/bin/linux/license-eye"
+
+echo $LICENSE_TOOL_PATH
 
 # TODO: DRY up the calls to the license tool
 
@@ -20,8 +15,8 @@ if [ "$LICENSE_TOOL_COMMAND" == "fix" ]; then
   $LICENSE_TOOL_PATH -c "$ROOT_REPO_DIRECTORY"/configs/apache2.yaml -v error header fix
   $LICENSE_TOOL_PATH -c "$ROOT_REPO_DIRECTORY"/configs/CC-BY-SA-4_0.yaml -v error header fix
 elif [ "$LICENSE_TOOL_COMMAND" == "check" ]; then
-  $LICENSE_TOOL_PATH -c "$ROOT_REPO_DIRECTORY"/configs/apache2.yaml -v error header check
-  $LICENSE_TOOL_PATH -c "$ROOT_REPO_DIRECTORY"/configs/CC-BY-SA-4_0.yaml -v error header check
+  $LICENSE_TOOL_PATH -c "$ROOT_REPO_DIRECTORY"/configs/apache2.yaml -v info header check
+  $LICENSE_TOOL_PATH -c "$ROOT_REPO_DIRECTORY"/configs/CC-BY-SA-4_0.yaml -v info header check
 else
     echo "Missing license check command -- Must be 'fix' or 'check'."
 fi
