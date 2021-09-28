2ad443af667a3e23f67e399ee21f6035c3c8ebc4
Forrest <forrest@lunasec.io>
2021-09-28 11:13:28 -0700

    got grant configurable time go code working
    Former-commit-id: b17fdc51cbde0d46dcd2cce2616f670f93454bcf
    Former-commit-id: 3feac94c86216e18eac21c450c1a66722ca8ce87

--- go/types/event/grant_set.go
+++ go/types/event/grant_set.go
@@ -17,7 +17,7 @@ package event
 type GrantSetRequest struct {
 	SessionID string `json:"sessionId"`
 	TokenID  string                 `json:"tokenId"`
-	CustomDuration int `json:"customDuration,omitempty"`
+	CustomDuration int `json:"customDuration"`
 }
 
 type GrantSetResponse struct {
