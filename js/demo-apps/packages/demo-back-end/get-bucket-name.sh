fdc75c99ea51e9d61eec7fa7474ab03e5bdbe6ee
Forrest <forrest@lunasec.io>
2021-09-29 18:44:54 -0700

    added debug for CI DSIOFNDSIOUFJOIUDF
    Former-commit-id: 699c6b11f0aa7fe493af1f00ca42ffb998dabdba
    Former-commit-id: 2bb6db2565613837405b13b566b8415db7236d15

--- js/demo-apps/packages/demo-back-end/get-bucket-name.sh
+++ js/demo-apps/packages/demo-back-end/get-bucket-name.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 # bucket names are random in localstack, so this script gets the bucket name from aws if none is set in the env vars
+echo "get-bucket-name.sh script started" >&2
 if [ "$CIPHERTEXT_S3_BUCKET" != "" ]; then
+  echo "Found s3 bucket in env variable as $CIPHERTEXT_S3_BUCKET, using it" >&2
   echo "$CIPHERTEXT_S3_BUCKET"
 else
   while :
   do
+    echo "Attempting to query aws localstack for bucket name and looping until success, please wait..." >&2
     name=$(aws --endpoint-url=http://localhost:4566 cloudformation describe-stacks --stack-name LunasecSecureEnclave --query "Stacks[0].Outputs[?ExportName==\`ciphertextbucketArnOutput\`].OutputValue" | jq -r ".[0]")
     if [ "$name" != "" ]; then
+      echo "Received bucket name from aws: $name" >&2
       break
     fi
   done
