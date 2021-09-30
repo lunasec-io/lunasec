3d716d48f0791644fe0194ceef70d76c975351ff
Forrest <forrest@lunasec.io>
2021-09-29 18:03:14 -0700

    race condition handled
    Former-commit-id: d449a58aff43d671cbd1467140437de46098c779
    Former-commit-id: 8ea595f2c1539584a85da2e5253142acf11ba2e5

--- js/demo-apps/packages/demo-back-end/get-bucket-name.sh
+++ js/demo-apps/packages/demo-back-end/get-bucket-name.sh
@@ -3,6 +3,12 @@
 if [ "$CIPHERTEXT_S3_BUCKET" != "" ]; then
   echo "$CIPHERTEXT_S3_BUCKET"
 else
-  name=$(aws --endpoint-url=http://localhost:4566 cloudformation describe-stacks --stack-name LunasecSecureEnclave --query "Stacks[0].Outputs[?ExportName==\`ciphertextbucketArnOutput\`].OutputValue" | jq -r ".[0]")
+  while :
+  do
+    name=$(aws --endpoint-url=http://localhost:4566 cloudformation describe-stacks --stack-name LunasecSecureEnclave --query "Stacks[0].Outputs[?ExportName==\`ciphertextbucketArnOutput\`].OutputValue" | jq -r ".[0]")
+    if [ "$name" != "" ]; then
+      break
+    fi
+  done
   echo "$name"
 fi
