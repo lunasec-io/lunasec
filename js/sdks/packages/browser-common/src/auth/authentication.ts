e129c0cbdb7c9d9f5ad556265abb06c4ce8db7b9
Forrest <forrest@lunasec.io>
2021-10-15 16:23:15 -0700

    fix auth tests and add to CI

--- js/sdks/packages/browser-common/src/auth/authentication.ts
+++ js/sdks/packages/browser-common/src/auth/authentication.ts
@@ -14,11 +14,12 @@
  * limitations under the License.
  *
  */
-import { SecureFrameAuthClient } from './auth-client';
+import { LunaSecError } from '@lunasec/isomorphic-common';
 
+import { SecureFrameAuthClient } from './auth-client';
 // This could probably be a local variable but just worried about any potential duplication between a custom app and our libraries makes the singletons a little sketchier
 
-// TODO: support env var refresh interval
+// TODO: support configurable refresh inverval
 // const refreshTimeInMinutes = process.env.LUNASEC_VERIFY_SESSION_INTERVAL
 //const refreshInterval = 5 * 60000; // 5 mins
 const refreshInterval = 1 * 60000; // 5 mins
@@ -37,14 +38,23 @@ export class LunaSecAuthentication {
 
   constructor(authDomain: string, errorHandler: (e: Error) => void, sessionAuthProvider?: string) {
     this.authClient = new SecureFrameAuthClient(authDomain);
+    if (!errorHandler) {
+      throw new LunaSecError({
+        code: '500',
+        name: 'errorHandlerUnset',
+        message: 'Must set error handler for LunaSec auth',
+      });
+    }
     this.errorHandler = errorHandler;
     this.sessionAuthProvider = sessionAuthProvider;
   }
 
   async startSessionManagement(): Promise<() => void> {
+    console.log('session management called');
     await this.authenticateSession();
 
     const timer = setInterval(() => {
+      console.log('timer called authenticate session for a second run');
       this.authenticateSession().catch((e) => {
         throw e;
       });
@@ -61,18 +71,18 @@ export class LunaSecAuthentication {
     if (authFlowInProgress) {
       if (lastAuthenticatedTime) {
         // If a request is in flight but we already had an older session, just resolve immediately
-        console.debug('previous request in flight but old session exists, returning instantly');
+        console.log('previous request in flight but old session exists, returning instantly');
         return;
       } else {
         // or if this is the first time and we dont have an older session, block until the session gets set
-        console.debug('previous request in flight and no old session, returning request promise');
+        console.log('previous request in flight and no old session, returning request promise');
         return authFlowInProgress;
       }
     }
-    console.debug('Start new auth flow');
+    console.log('authenticate session not skipping');
 
     if (!lastAuthenticatedTime || lastAuthenticatedTime < Date.now() - refreshInterval) {
-      console.debug('need to fetch new session');
+      console.log('need to fetch new session');
       authFlowInProgress = this.validateOrCreateSession();
       const authSuccess = await authFlowInProgress;
       if (authSuccess) {
@@ -80,28 +90,26 @@ export class LunaSecAuthentication {
       }
       authFlowInProgress = null;
     } else {
-      console.debug('authenticated recently enough to skip the auth flow');
+      console.log('authenticated recently enough to skip the auth flow');
     }
   }
 
+  /**
+    Ensure session will first call into the secure frame to create an auth flow state session
+    it will then redirect to the auth provider to verify the user's identity
+    the auth provider will then redirect back to the secure frame's '/session/create' endpoint
+    this will finalize the authentication request and set an 'access_token' cookie on the secure frame origin
+
+    Because we are redirecting cross origin, ensure session will not have a response as it is set to execute
+    with 'no-cors' set. It is possible to experience an error in this request flow.
+   */
   async validateOrCreateSession(): Promise<boolean> {
-    /*
-      If the above was not a success, we need to send more requests to verify the session.
-
-      Ensure session will first call into the secure frame to create an auth flow state session
-      it will then redirect to the auth provider to verify the user's identity
-      the auth provider will then redirect back to the secure frame's '/session/create' endpoint
-      this will finalize the authentication request and set an 'access_token' cookie on the secure frame origin
-
-      Because we are redirecting cross origin, ensure session will not have a response as it is set to execute
-      with 'no-cors' set. It is possible to experience an error in this request flow.
-     */
-    console.debug('ensuring the secure frame has a valid session');
+    console.log('ensuring the secure frame has a valid session');
 
     // We can get NO information back from this call because of CORS, so we have to call again afterwards
     await this.authClient.ensureSession(this.sessionAuthProvider);
 
-    console.debug('verifying the created session is valid');
+    console.log('verifying the created session is valid');
     const verifyResponse = await this.authClient.verifySession();
     if (!verifyResponse.success) {
       this.errorHandler(verifyResponse.error);
