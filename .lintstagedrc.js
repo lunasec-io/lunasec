6124d573aadf957ca84e1413535ec8d41656a460
Free Wortley <free@lunasec.io>
2021-10-18 21:05:16 -0500

    Make lint-stage script have header

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,8 +1,24 @@
+/*
+ * Copyright 2021 by LunaSec (owned by Refinery Labs, Inc)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 const fs = require('fs');
 const uuid = require('uuid').v4;
 const yaml = require('js-yaml');
 
-const javascriptRegex = /\.[jt]sx?$/i;
+const javascriptRegex = /\.(cj|mj|j|t)sx?$/i;
 const golangRegex = /\.go$/i;
 const markdownRegex = /\.mdx?$/i;
 
@@ -132,7 +148,7 @@ module.exports = (allStagedFiles) => {
   // Lint all JS files
   const jsFiles = allStagedFiles.filter(file => file.match(javascriptRegex));
   if (jsFiles.length > 0) {
-    outputCommands.push(`eslint --fix ${jsFiles.join(' ')}`);
+    outputCommands.push(`sh -c "cd js/sdks && eslint --fix ${jsFiles.join(' ')}"`);
   }
 
   return outputCommands;
