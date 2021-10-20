b28dac17e5bd339109d08e6fe295e273d540de49
Forrest <forrest@lunasec.io>
2021-10-19 17:28:22 -0700

    test license checking vue files

--- js/sdks/packages/vue-sdk/src/App.vue
+++ js/sdks/packages/vue-sdk/src/App.vue
@@ -2,7 +2,7 @@
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
 </template>
-
+<!--test change-->
 <script lang="ts">
 import { defineComponent } from "vue";
 import HelloWorld from "./components/HelloWorld.vue";
