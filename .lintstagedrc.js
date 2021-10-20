20cccfe649291916214cac382b5a83520e34ff6b
Forrest <forrest@lunasec.io>
2021-10-19 17:33:34 -0700

    tried to match vue files but not working with skywalking eyes
    Former-commit-id: 50db78aaed0a868bf97da544a062545f650b2222
    Former-commit-id: e8b2f82cc6261c0b5934f0ad8d0827b2cde60f7e

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -24,7 +24,7 @@ const yaml = require('js-yaml');
  * If you know of a way to clean it up, feel free to. This seemed like the simplest solution though.
  * @type {RegExp}
  */
-const javascriptRegex = /\.((cj|mj|j|t)sx?)vue$/i;
+const javascriptRegex = /\.(cj|mj|j|t)sx?$/i; // todo: match .vue files
 const golangRegex = /\.go$/i;
 const markdownRegex = /\.mdx?$/i;
 
