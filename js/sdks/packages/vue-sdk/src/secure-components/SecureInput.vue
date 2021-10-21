42dbe847b0ae0ec04a56b5d59b5a7eb584050d40
Forrest <forrest@lunasec.io>
2021-10-20 18:15:21 -0700

    setupSecureComponent vue function is getting ref
    Former-commit-id: 8b02a2aec01766276a8750d5ae51037ab642f629
    Former-commit-id: bf17842b5c3dbc4c245de4b9f27696846a842a2e

--- js/sdks/packages/vue-sdk/src/secure-components/SecureInput.vue
+++ js/sdks/packages/vue-sdk/src/secure-components/SecureInput.vue
@@ -1,5 +1,5 @@
 <template>
-  <input />
+  <input ref="wrappedElementRef" />
 </template>
 
 <script lang="ts">
@@ -10,27 +10,12 @@ export default defineComponent({
   props: {
     msg: String,
   },
-  setup(props) {
-    setupSecureComponent(this.$refs.);
-
+  setup() {
+    return setupSecureComponent();
   },
+  methods: {},
 });
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+<style scoped></style>
