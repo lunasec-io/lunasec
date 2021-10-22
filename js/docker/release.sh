60d56f624fc2c8502957b889a6ada564eddad218
breadchris <chris@lunasec.io>
2021-10-22 01:37:53 -0400

    fix release script names

--- js/docker/release.sh
+++ js/docker/release.sh
@@ -3,9 +3,9 @@
 export DOCKER_BUILDKIT=1
 
 if [ "$1" == "build" ]; then
-  docker build -f js/docker/dependency-precache.dockerfile . -t "lunasec/cached-npm-dependencies:${VERSION}"
-  docker build -f js/docker/demo.dockerfile . --target demo-back-end -t "lunasec/application-back-end-demo:${VERSION}"
-  docker build -f js/docker/demo.dockerfile . --target react-front-end -t "lunasec/application-front-end-demo:${VERSION}"
+  # docker build -f js/docker/dependency-precache.dockerfile . -t "lunasec/cached-npm-dependencies:${VERSION}"
+  docker build -f js/docker/demo.dockerfile . --target application-front-end -t "lunasec/application-front-end-demo:${VERSION}"
+  docker build -f js/docker/demo.dockerfile . --target application-back-end -t "lunasec/application-back-end-demo:${VERSION}"
   docker build -f js/docker/demo.dockerfile . --target secure-frame-iframe -t "lunasec/secure-frame-iframe-demo:${VERSION}"
   docker build -f js/docker/demo.dockerfile . --target lunasec-cli -t "lunasec/lunasec-cli-demo:${VERSION}"
   docker build -f js/docker/httpsproxy.dockerfile . -t "lunasec/localstack-proxy-demo:${VERSION}"
@@ -13,7 +13,7 @@ if [ "$1" == "build" ]; then
 fi
 
 if [ "$1" == "publish" ]; then
-  docker push "lunasec/cached-npm-dependencies:${VERSION}"
+  # docker push "lunasec/cached-npm-dependencies:${VERSION}"
   docker push "lunasec/application-back-end-demo:${VERSION}"
   docker push "lunasec/application-front-end-demo:${VERSION}"
   docker push "lunasec/secure-frame-iframe-demo:${VERSION}"
