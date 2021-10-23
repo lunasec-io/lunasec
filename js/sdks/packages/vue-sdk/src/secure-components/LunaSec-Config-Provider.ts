ede7a4c93d00a19394b04c0582a0b7ec46f59770
Forrest <forrest@lunasec.io>
2021-10-22 17:14:56 -0700

    lint changes done
    Former-commit-id: a9a022d1f720db4a2022ff75447dea0992d06adc
    Former-commit-id: 0359d0b502e07494af8004db07038aa204da5011

---
+++ js/sdks/packages/vue-sdk/src/secure-components/LunaSec-Config-Provider.ts
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2021 by LunaSec (owned by Refinery Labs, Inc)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+export interface LunaSecConfigProviderAttrs {
+  lunaSecDomain: string;
+  sessionAuthProvider: string;
+  authenticationErrorHandler: (e: Error) => void;
+}
+import { defineComponent, provide, toRefs } from 'vue';
+export default defineComponent({
+  name: 'Demo',
+  props: {
+    lunaSecDomain: {
+      type: String,
+      required: true,
+    },
+    sessionAuthProvider: String,
+    authenticationErrorHandler: {
+      type: Function, // as (e: Error) => void,
+      default: (e: Error) => {
+        throw e;
+      },
+    },
+  },
+  setup: (props) => {
+    provide('lunaSecConfig', toRefs(props));
+  },
+});
