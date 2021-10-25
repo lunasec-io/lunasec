12b4aef72e4e24123492dd5edb756e388b27436e
Forrest <forrest@lunasec.io>
2021-10-24 17:47:20 -0700

    File handling inside tokenizer (#167)
    * began moving file handling into tokenizer
    
    * file refactor complete, tokenizer tests need rewrite
    
    * add tokenizer file tests
    
    * free fixes
    
    Former-commit-id: 356edc84958a26ca531d6652b79e6da74dc77827
    Former-commit-id: d3e4eab9962691e5ab9f35ed5725e690e0528b14

--- js/sdks/packages/tokenizer-sdk/jest.config.js
+++ js/sdks/packages/tokenizer-sdk/jest.config.js
@@ -17,6 +17,6 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
   modulePathIgnorePatterns: ["build"]
 };
