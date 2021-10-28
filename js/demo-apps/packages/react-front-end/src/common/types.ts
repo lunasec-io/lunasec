43740e3840bced6ceaa74d29590af3f62ff8bb3b
Free Wortley <free@lunasec.io>
2021-10-27 20:10:05 -0500

    Add long scary comment
    Former-commit-id: 4729e2a3aee8246521697b0bd61172894bad1f47
    Former-commit-id: 58adc7c73d78427d09a74d7d048ed70ed704e56c

--- js/demo-apps/packages/react-front-end/src/common/types.ts
+++ js/demo-apps/packages/react-front-end/src/common/types.ts
@@ -48,6 +48,29 @@ interface UserDocumentsSuccess {
 
 export type UserDocumentsResponse = UserDocumentsSuccess | FailApiResponse;
 
+/**
+ These strings here are used to define Thunks in the store. Whenever you add a new Thunk, you must add it here.
+
+ This implementation is... a little complicated.
+ It's a clever solution that's type-safe, but that's also what I don't like about it.
+
+ We need to have only one store if we want the app to work well.
+ That means we need 2 functions -- one for the Express and GraphQL implementations.
+
+ But how do manage that? Do we now have every component in the app aware of the "mode" that the app runs in?
+ That's a leaky abstraction!
+
+ So the solution is to create a standard "actions" object that is passed to every component of the app.
+ Components can define which actions they need in their props.
+
+ Then we "map" the desired actions (express or graphql) into the object at runtime based on the mode.
+ But, how do we ensure that every time an action is added that it's properly statically typed?
+
+ Well, to do that we follow a normal convention for the function. Like this: `saveSsn` and `saveSsnGql`.
+
+ To enforce this, I'm using the TypeScript Template literals functionality in the `types.ts` file to enforce that these
+ keys are present on the store implementation. It works beautifully, but the syntax is quite terse.
+ */
 export type StoreThunks = 'saveSsn' | 'loadUser' | 'loadDocuments' | 'uploadDocumentTokens' | 'login' | 'signup';
 export type StoreModes = '' | 'gql';
 
