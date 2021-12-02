b19c42c7095327b897348ec70a91dfca6adfb93b
breadchris <chris@lunasec.io>
2021-12-01 19:29:14 -0500

    replace yargs with cmd-ts

--- go/scripts/start-tokenizerbackend-dependencies.sh
+++ go/scripts/start-tokenizerbackend-dependencies.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo rm -f ./outputs/aws_resources.json
 
-npx lunasec start --local-build=true --env=local-dependencies
+npx lunasec start --local-build --env=local-dependencies
 r=$?
 if [ $r -ne 0 ]; then
     echo "Unable to bring up local dependencies for the tokenizer"
