457d281ca31b7ed265d4e030e8955e5c009f540a
Free Wortley <free@lunasec.io>
2021-12-12 17:51:56 -0600

    Fix script to work with both a specific path or in the current folder

--- js/sdks/packages/log4shell/entrypoint.sh
+++ js/sdks/packages/log4shell/entrypoint.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
-shift 2
+CURRENT_PATH=$(pwd)
+
+# We must resolve the symlink in order to get the path to the NPM module
+REAL_PATH=$(readlink -f "$0" | sed 's/entrypoint.sh//')
+
+cd $REAL_PATH
+
+#shift 1
 cmd="$@"
 
-npm run $1
+# If the user passed us a path, then we will pass that along instead.
+if [[ -n "$2" ]]; then
+  npm run $1 $2
+else
+  npm run $1 $CURRENT_PATH
+fi
+
--- js/sdks/packages/log4shell/find-bad-deps.sh
+++ js/sdks/packages/log4shell/find-bad-deps.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-BAD_PACKAGES=$(find . -iname "*.jar" | xargs -I% sh -c "sha256sum % | cut -c1-64 | xargs -I^ grep -q ^ ./CVE-2021-44228-Log4Shell-Hashes/sha256sums.txt && echo %")
+if [[ -z $1 ]]; then
+  echo "Must provide a path to the folder to check deps for."
+  exit 1
+fi
+
+CVE_HASH_PATH="$(pwd)/CVE-2021-44228-Log4Shell-Hashes/sha256sums.txt"
+
+cd $1
+
+BAD_PACKAGES=$(find . -iname "*.jar" | xargs -I% sh -c "sha256sum % | cut -c1-64 | xargs -I^ grep -q ^ $CVE_HASH_PATH && echo %")
 
 if [[ -z $BAD_PACKAGES ]]; then
   echo "No bad packages were found with known any hashes."
