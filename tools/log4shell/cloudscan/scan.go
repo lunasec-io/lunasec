47fad5769f8a8fae3f67105d6c923d2754883cb6
Johnathan Free Wortley <free@lunasec.io>
2022-01-11 03:00:03 -0600

    LunaTrace CLI Backend MVP (#412)
    * LunaTrace CLI Backend MVP
    
    * Add metadata to upload endpoint
    
    * Update permissions
    
    * Bump deps
    
    * base64 encode s3 url
    
    * Encode the characters and drop the base64
    
    * Log4shell sbom processing (#414)
    
    * add cloud-scan command to log4shell cli
    
    * break out cloudscan components into different files
    
    * slugify the s3 id
    
    * cataloged packages are sent to s3
    
    Co-authored-by: Chris <chris@lunasec.io>
    Former-commit-id: 6e856d6399b98a2d39df8fd8430bcc825a3adb18
    Former-commit-id: 2b97c3e9ce43a0514329140ba40a5c21f1c44f56

---
+++ tools/log4shell/cloudscan/scan.go
@@ -0,0 +1,46 @@
+// Copyright 2022 by LunaSec (owned by Refinery Labs, Inc)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+package cloudscan
+
+import (
+	"github.com/anchore/syft/syft/sbom"
+	"github.com/rs/zerolog/log"
+	"path/filepath"
+)
+
+func CollectSbomsFromSearchDirs(searchDirs, excludedDirs []string) (sboms []*sbom.SBOM, err error) {
+	for _, searchDir := range searchDirs {
+		var (
+			s          *sbom.SBOM
+			searchPath string
+		)
+
+		searchPath, err = filepath.Abs(searchDir)
+		if err != nil {
+			return
+		}
+
+		s, err = getSbomForSearchDir(searchPath, excludedDirs)
+		if err != nil {
+			log.Error().
+				Str("searchPath", searchPath).
+				Err(err).
+				Msg("Unable to create SBOM from directory.")
+			return
+		}
+		sboms = append(sboms, s)
+	}
+	return
+}
