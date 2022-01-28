50622a6e6d0b46f727e6e2bdb7a40baabaa10358
breadchris <chris@lunasec.io>
2022-01-27 20:31:44 -0800

    move frontend and hasura into bsl
    Former-commit-id: 42d8b7dab63a4b095d69c23c69f47fb196942795
    Former-commit-id: 8c0c59b7272597e6706d5e2294961e79a4e25998

---
+++ lunatrace/bsl/frontend/src/constants.ts
@@ -0,0 +1,90 @@
+/*
+ * Copyright by LunaSec (owned by Refinery Labs, Inc)
+ *
+ * Licensed under the Business Source License v1.1
+ * (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ * https://github.com/lunasec-io/lunasec/blob/master/licenses/BSL-LunaTrace.txt
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+/*
+ * Copyright 2022 by LunaSec (owned by Refinery Labs, Inc)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+export const SIDEBAR_POSITION = {
+  LEFT: 'left',
+  RIGHT: 'right',
+};
+
+export const SIDEBAR_BEHAVIOR = {
+  STICKY: 'sticky',
+  FIXED: 'fixed',
+  COMPACT: 'compact',
+};
+
+export const LAYOUT = {
+  FLUID: 'fluid',
+  BOXED: 'boxed',
+};
+
+export const THEME = {
+  DEFAULT: 'dark',
+  COLORED: 'colored',
+  DARK: 'dark',
+  LIGHT: 'light',
+};
+
+export const THEME_PALETTE_LIGHT = {
+  primary: '#3B82EC',
+  'primary-dark': '#1659c7',
+  'primary-light': '#84aef2',
+  secondary: '#495057',
+  success: '#4BBF73',
+  info: '#1F9BCF',
+  warning: '#f0ad4e',
+  danger: '#d9534f',
+  white: '#fff',
+  'gray-100': '#f4f7f9',
+  'gray-200': '#e2e8ee',
+  'gray-300': '#dee6ed',
+  'gray-400': '#ced4da',
+  'gray-500': '#adb5bd',
+  'gray-600': '#6c757d',
+  'gray-700': '#495057',
+  'gray-800': '#020202',
+  'gray-900': '#212529',
+  black: '#000',
+};
+
+export const THEME_PALETTE_DARK = {
+  ...THEME_PALETTE_LIGHT,
+  'primary-dark': '#84aef2',
+  'primary-light': '#1659c7',
+  white: '#293042',
+  'gray-100': '#3e4555',
+  'gray-200': '#545968',
+  'gray-300': '#696e7b',
+  'gray-400': '#7f838e',
+  'gray-500': '#9498a1',
+  'gray-600': '#a9acb3',
+  'gray-700': '#bfc1c6',
+  'gray-800': '#d4d6d9',
+  'gray-900': '#eaeaec',
+  black: '#fff',
+};
