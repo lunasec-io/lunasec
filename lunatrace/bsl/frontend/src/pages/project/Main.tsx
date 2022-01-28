3ee23691b23a9d2b5556f0849dedf634db9d06fe
Forrest <forrest@lunasec.io>
2022-01-28 14:39:31 -0800

    copied over changes and created submodule

--- lunatrace/bsl/frontend/src/pages/project/Main.tsx
+++ lunatrace/bsl/frontend/src/pages/project/Main.tsx
@@ -11,22 +11,6 @@
  * limitations under the License.
  *
  */
-/*
- * Copyright 2022 by LunaSec (owned by Refinery Labs, Inc)
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
 import React, { useState } from 'react';
 import { Container, Nav, Row } from 'react-bootstrap';
 import { Box, Settings } from 'react-feather';
@@ -48,6 +32,8 @@ export const ProjectMain: React.FunctionComponent = (_props) => {
   const { data, error, isLoading } = useGetProjectQuery({
     project_id,
   });
+
+  console.log('fetched data from hasura ', data);
 
   const [activeTab, setActiveTab] = useState<'builds' | 'settings'>('builds');
   const renderProjectNav = (p: ProjectInfo) => {
