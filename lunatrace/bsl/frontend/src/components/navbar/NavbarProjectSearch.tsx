9b0f228e1be15acedc6ffa38a9b3306155509d0d
Forrest <forrest@lunasec.io>
2022-01-30 12:31:39 -0800

    fancy global project search

--- lunatrace/bsl/frontend/src/components/navbar/NavbarProjectSearch.tsx
+++ lunatrace/bsl/frontend/src/components/navbar/NavbarProjectSearch.tsx
@@ -13,12 +13,38 @@
  */
 import React from 'react';
 import { Button, Form, InputGroup } from 'react-bootstrap';
+import { Typeahead } from 'react-bootstrap-typeahead';
+import { Option } from 'react-bootstrap-typeahead/types/types';
 import { Search } from 'react-feather';
+import { useNavigate } from 'react-router-dom';
+
+import { GetSidebarInfoQuery, useGetSidebarInfoQuery } from '../../store/api/generated';
+
 export const ProjectSearch: React.FunctionComponent = () => {
+  const navigate = useNavigate();
+  // Just reuse the same query from the sidebar despite the overfetch, because it will be cached
+  const { data, error, isLoading } = useGetSidebarInfoQuery();
+
+  const handleProjectSelected = (options: Option[]) => {
+    const selected = options[0] as GetSidebarInfoQuery['projects'][number] | undefined;
+    if (!selected) {
+      return;
+    }
+    navigate(`/project/${selected.id as string}`);
+  };
   return (
     <Form inline="true" className="d-none d-sm-inline-block">
       <InputGroup className="input-group-navbar">
-        <Form.Control placeholder="Search Projects" aria-label="Search Projects" />
+        <Typeahead
+          id="project-search-form"
+          placeholder="Search Projects"
+          aria-label="Search Projects"
+          onChange={handleProjectSelected}
+          labelKey="name"
+          options={!data ? [] : data.projects}
+          highlightOnlyResult={true}
+          selectHintOnEnter={true}
+        />
         <Button variant="">
           <Search className="feather" />
         </Button>
