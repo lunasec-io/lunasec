3dca2f85374705e6e06f48fd1cf787451fdc6449
Forrest <forrest@lunasec.io>
2022-02-05 13:58:28 -0800

    pnp intellij not working

--- lunatrace/bsl/frontend/src/pages/project/BuildDetails.tsx
+++ lunatrace/bsl/frontend/src/pages/project/BuildDetails.tsx
@@ -1,19 +1,33 @@
+/*
+ * Copyright by LunaSec (owned by Refinery Labs, Inc)
+ *
+ * Licensed under the Business Source License v1.1
+ * (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ * https://github.com/lunasec-io/lunasec/blob/master/licenses/BSL-LunaTrace.txt
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 import React from 'react';
-import { Container Col } from 'react-bootstrap';
-import {useGetBuildDetailsQuery, GetBuildDetailsQuery} from "../../store/api/generated";
-import {Spinner} from "react-bootstrap";
+import { Col, Container, Spinner } from 'react-bootstrap';
+
+import { GetBuildDetailsQuery, useGetBuildDetailsQuery } from '../../store/api/generated';
 
 export interface BuildDetailsProps {
   buildId: string;
 }
+
 export const BuildDetails: React.FunctionComponent<BuildDetailsProps> = ({ buildId }) => {
-  const {data, error, isLoading} = useGetBuildDetailsQuery({build_id:buildId})
+  const { data, error, isLoading } = useGetBuildDetailsQuery({ build_id: buildId });
   return (
-      {isLoading ? <Spinner animation="border" variant="primary" /> : <Container className="build-page">
+    <Container className="build-page">
       <Col xs="12" className="d-none d-sm-block" style={{ textAlign: 'center' }}>
         <h1>{project.name}</h1>
         <br />
       </Col>
-    </Container>}
+    </Container>
   );
 };
