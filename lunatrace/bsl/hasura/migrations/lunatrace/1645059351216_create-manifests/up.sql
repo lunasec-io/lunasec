b9b361e955844b23b5f2c45d43843ee746e8d529
Forrest <forrest@lunasec.io>
2022-02-16 17:15:27 -0800

    condense migrations
    Former-commit-id: dfd7c5159c18cfe67710e2898134d987303006d1
    Former-commit-id: 349e52a08475d49aba26a70a0f279cd7db471af9

--- lunatrace/bsl/hasura/migrations/lunatrace/1645059351216_create-manifests/up.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE public.manifests
-(
-    id         uuid                        DEFAULT public.gen_random_uuid() NOT NULL PRIMARY KEY,
-    created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    project_id uuid references public.projects (id) NOT NULL,
-    s3_url     text                                 NOT NULL UNIQUE,
-    filename   text                                 NOT NULL
-)
