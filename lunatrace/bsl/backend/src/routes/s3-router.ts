a182116f64e30aa32e9e8701e2a3dbd45bda1377
Forrest <forrest@lunasec.io>
2022-02-16 17:12:40 -0800

    uploaded manifests work and save in hasura

--- lunatrace/bsl/backend/src/routes/s3-router.ts
+++ lunatrace/bsl/backend/src/routes/s3-router.ts
@@ -1,3 +1,16 @@
+/*
+ * Copyright by LunaSec (owned by Refinery Labs, Inc)
+ *
+ * Licensed under the Business Source License v1.1
+ * (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ * https://github.com/lunasec-io/lunasec/blob/master/licenses/BSL-LunaTrace.txt
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 import { defaultProvider } from '@aws-sdk/credential-provider-node';
 import express from 'express';
 import { v4 as uuid } from 'uuid';
@@ -17,15 +30,17 @@ if (!awsRegion || !manifestBucket) {
 // @ts-ignore
 // eslint-disable-next-line @typescript-eslint/no-misused-promises
 s3Router.post('/presign-manifest-upload', async (req, res) => {
+  console.log('PRESIGN ROUTE CALLED');
   const preSignedUrlGenerator = new PreSignedUrlGenerator({
     awsCredentials: defaultProvider(),
     awsRegion,
     s3Bucket: manifestBucket,
   });
-
-  const projectId = req.body.project_id as string | undefined;
+  console.log('created s3 generator');
+  const projectId = req.body.input.project_id as string | undefined;
+  console.log('request body is ', req.body);
   if (!projectId) {
-    return res.status(400).send({ error: 'Missing project_id in request' });
+    return res.status(400).send({ error: true, message: 'Missing project_id in request' });
   }
 
   const today = new Date();
@@ -34,11 +49,14 @@ s3Router.post('/presign-manifest-upload', async (req, res) => {
   const objectKey = `${encodeURIComponent(projectId)}
   /${today.getFullYear()}/${today.getMonth()}/${today.getDay()}/${today.getHours()}
   /${encodeURIComponent(uniqueId)}`;
+  console.log('about to try');
 
   try {
+    console.log('attempting to contact s3');
     const result = await preSignedUrlGenerator.generatePresignedS3Url(objectKey, 'PUT');
-    return res.json({ error: false, uploadUrl: result });
+    console.log('s3 responded ', result);
+    return res.json({ error: false, ...result });
   } catch (e) {
-    return res.status(400).json({ error: 'Failed to generate S3 presigned url' });
+    return res.status(400).json({ error: true, message: 'Failed to generate S3 presigned url' });
   }
 });
--- lunatrace/bsl/frontend/src/pages/project/dashboard/ManifestDrop.tsx
+++ lunatrace/bsl/frontend/src/pages/project/dashboard/ManifestDrop.tsx
@@ -1,21 +1,66 @@
+/*
+ * Copyright by LunaSec (owned by Refinery Labs, Inc)
+ *
+ * Licensed under the Business Source License v1.1
+ * (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ * https://github.com/lunasec-io/lunasec/blob/master/licenses/BSL-LunaTrace.txt
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+import axios, { AxiosRequestHeaders } from 'axios';
 import React, { useEffect } from 'react';
 import { Card } from 'react-bootstrap';
 import { DropzoneOptions, useDropzone } from 'react-dropzone';
 
-import { usePresignManifestUrlMutation } from '../../../store/api/generated';
+import { useInsertManifestMutation, usePresignManifestUrlMutation } from '../../../store/api/generated';
+
+const axiosInstance = axios.create();
 
 export const ManifestDrop: React.FunctionComponent<{ project_id: string }> = ({ project_id }) => {
-  console.log('rendering dropzone');
-  const onDrop: DropzoneOptions['onDrop'] = (acceptedFiles) => {
-    console.log(acceptedFiles);
+  console.log('rendering dropzone for project id ', project_id);
+
+  const [generatePresignedUrl] = usePresignManifestUrlMutation();
+  const [insertManifest] = useInsertManifestMutation();
+
+  const onDrop: DropzoneOptions['onDrop'] = async (acceptedFiles) => {
+    console.log('File dropped! ', acceptedFiles);
+    const file = acceptedFiles[0];
+
+    if (!file) return;
+
+    const presignRequest = await generatePresignedUrl({ project_id }).unwrap(); //unwrap turns this into an inline call
+    const presignResult = presignRequest.presignManifestUpload;
+    if (!presignResult) {
+      console.error('failed presigning url');
+      return;
+    }
+    console.log('presign result ', presignResult);
+    const options = {
+      headers: {
+        'Content-Type': file.type,
+      },
+    };
+
+    const uploadResult = await axios.put(presignResult.url, file, options);
+    console.log('upload success ', uploadResult.data);
+    const manifestUrl = presignResult.url.split('?')[0];
+    console.log('new file is at ', manifestUrl);
+
+    const insertRequest = await insertManifest({ s3_url: manifestUrl, project_id, filename: file.name }).unwrap();
+    if (!insertRequest.insert_manifests_one) {
+      console.error('Failed to notify lunatrace up uploaded manifest');
+      return;
+    }
+    const manifestId = insertRequest.insert_manifests_one.id;
+    console.log('manifest id is ', manifestId);
+    // return axios.put(signedUrl, file, options);
   };
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
 
-  const [generatePresignedUrl, presignedUrlResult] = usePresignManifestUrlMutation();
-  useEffect(() => {
-    generatePresignedUrl({ project_id });
-  }, []);
-  console.log('result is ', presignedUrlResult);
   return (
     <Card variant="dark" className="clickable-card text-center" {...getRootProps()}>
       <input {...getInputProps()} />
