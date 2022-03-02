73b6d7f5a067c6947d2e6dc4e39ff6337ce0480a
Forrest <forrest@lunasec.io>
2022-03-02 11:34:25 -0800

    lock to typescript 4.5

--- lunatrace/bsl/backend/src/handlers/generateSbom.ts
+++ lunatrace/bsl/backend/src/handlers/generateSbom.ts
@@ -1,3 +1,16 @@
+/*
+ * Copyright by LunaSec (owned by Refinery Labs, Inc)
+ *
+ * Licensed under the Business Source License v1.1
+ * (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ * https://github.com/lunasec-io/lunasec/blob/master/licenses/BSL-LunaTrace.txt
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 import fs from 'fs';
 import path from 'path';
 import util from 'util';
@@ -12,10 +25,10 @@ export async function handleGenerateSbom(message: SqsMessage) {
   const key = message.s3.object.key;
   try {
     const hasuraRes = await fetchSetManifestStatus(key, 'processing');
-
     console.log('filename is  ', hasuraRes);
     const fileBuffer = await aws.getFileFromS3(key, bucket);
     const fileString = await aws.streamToString(fileBuffer);
+    console.log('filestring is ', fileString);
     //todo: possible security issue with taking filename from client and writing it to disk
     const filePath = path.resolve(`./tmp/${hasuraRes.filename}`);
     fs.writeFileSync(filePath, fileString);
