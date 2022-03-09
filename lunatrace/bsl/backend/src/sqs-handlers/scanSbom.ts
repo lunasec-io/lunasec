eb85d32d7cc8164b62426448d1b068812134c155
Forrest <forrest@lunasec.io>
2022-03-09 11:55:15 -0800

    queue processor changes

--- lunatrace/bsl/backend/src/sqs-handlers/scanSbom.ts
+++ lunatrace/bsl/backend/src/sqs-handlers/scanSbom.ts
@@ -11,17 +11,22 @@
  * limitations under the License.
  *
  */
+import zlib from 'zlib';
+
 import validate from 'validator';
 
 import { hasura } from '../hasura-api';
 import { Scan } from '../models/scan';
 import { S3ObjectMetadata } from '../types/s3';
 import { aws } from '../utils/aws-utils';
+const gunzip = zlib.createGunzip();
+
 export async function handleScanSbom(message: S3ObjectMetadata) {
   try {
     console.log('scanning sbom ', message);
     const { key, region, bucketName } = message;
     const sbomStream = await aws.getFileFromS3(key, bucketName, region);
+    const unZippedSbomStream = sbomStream.pipe(gunzip);
     console.log('got file from s3');
     const buildId = key.split('/').pop();
     if (!buildId || !validate.isUUID(buildId)) {
@@ -29,8 +34,10 @@ export async function handleScanSbom(message: S3ObjectMetadata) {
       throw new Error('Build ID parsed from s3 key is not a valid uuid');
     }
     await hasura.UpdateManifestStatusIfExists({ status: 'scanning', buildId: buildId });
-    await Scan.uploadScan(sbomStream, buildId);
+    await Scan.uploadScan(unZippedSbomStream, buildId);
+    console.log('upload complete, notifying manifest if one exists');
     await hasura.UpdateManifestStatusIfExists({ status: 'scanned', buildId: buildId });
+    console.log('done with scan');
   } catch (e) {
     console.error(e);
     // todo: somehow write this error back to the db
