572534f56f2c5aa45143eea5db7ebb4f9e76a6b2
Free Wortley <free@lunasec.io>
2022-03-28 19:12:16 -0500

    Update generate TS

--- lunatrace/bsl/backend/src/hasura-api/generated.ts
+++ lunatrace/bsl/backend/src/hasura-api/generated.ts
@@ -1,3 +1,16 @@
+/*
+ * Copyright by LunaSec (owned by Refinery Labs, Inc)
+ *
+ * Licensed under the Business Source License v1.1 
+ * (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ * https://github.com/lunasec-io/lunasec/blob/master/licenses/BSL-LunaTrace.txt
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 import { GraphQLClient } from 'graphql-request';
 import * as Dom from 'graphql-request/dist/types.dom';
 import gql from 'graphql-tag';
@@ -72,8 +85,6 @@ export type ScanManifestOutput = {
   build_id: Scalars['String'];
   error: Scalars['Boolean'];
   error_message?: Maybe<Scalars['String']>;
-  /** An object relationship */
-  scan_build_id_relationship: Builds;
 };
 
 /** Boolean expression to compare columns of type "String". All fields are combined with logical 'AND'. */
