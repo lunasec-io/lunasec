48ada8abd4048b7aad8afd148c1807002122b93a
Free Wortley <free@lunasec.io>
2022-03-29 02:38:59 -0500

    CDK changes necessary for LunaTrace deploy to succeed

--- lunatrace/bsl/backend-cdk/lib/etl-storage-stack.ts
+++ lunatrace/bsl/backend-cdk/lib/etl-storage-stack.ts
@@ -14,13 +14,20 @@
 import { Bucket, HttpMethods } from '@aws-cdk/aws-s3';
 import { Queue } from '@aws-cdk/aws-sqs';
 import * as cdk from '@aws-cdk/core';
-import { Duration } from '@aws-cdk/core';
+import { Construct, Duration } from '@aws-cdk/core';
 
 interface EtlStorageStackProps extends cdk.StackProps {
   publicBaseUrl: string;
 }
 
-export class EtlStorageStack extends cdk.Stack {
+export interface EtlStorageStackState {
+  sbomBucket: Bucket;
+  manifestBucket: Bucket;
+  processManifestSqsQueue: Queue;
+  processSbomSqsQueue: Queue;
+}
+
+export class EtlStorageStack extends cdk.Stack implements EtlStorageStackState {
   public sbomBucket: Bucket;
   public manifestBucket: Bucket;
   public processManifestSqsQueue: Queue;
@@ -29,11 +36,28 @@ export class EtlStorageStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props: EtlStorageStackProps) {
     super(scope, id, props);
 
+    const stackState = EtlStorageStack.createEtlStorageStack(this, props);
+
+    this.sbomBucket = stackState.sbomBucket;
+    this.manifestBucket = stackState.manifestBucket;
+    this.processManifestSqsQueue = stackState.processManifestSqsQueue;
+    this.processSbomSqsQueue = stackState.processSbomSqsQueue;
+  }
+
+  /**
+   * This static because multi-stack CloudFormation is very complicated and full of pitfalls.
+   * See these articles for some info:
+   * https://stackoverflow.com/questions/62989129/resolution-error-cannot-use-resource-x-in-a-cross-environment-fashion-the-re
+   * https://awsmaniac.com/sharing-resources-in-aws-cdk/
+   * @param context The `this` context of an AWS CDK Stack. This must provide variables for re-assignment so that the "stack" values can be exported.
+   * @param props Variables required for the stack to deploy properly.
+   */
+  public static createEtlStorageStack(context: Construct, props: EtlStorageStackProps): EtlStorageStackState {
     const { publicBaseUrl } = props;
 
-    this.sbomBucket = new Bucket(this, 'SbomBucket');
+    const sbomBucket = new Bucket(context, 'SbomBucket');
 
-    this.manifestBucket = new Bucket(this, 'ManifestBucket', {
+    const manifestBucket = new Bucket(context, 'ManifestBucket', {
       cors: [
         {
           allowedMethods: [HttpMethods.GET, HttpMethods.PUT],
@@ -43,11 +67,11 @@ export class EtlStorageStack extends cdk.Stack {
       ],
     });
 
-    const processManifestDeadLetterQueue = new Queue(this, 'ProcessManifestProcessingDeadLetterQueue', {
+    const processManifestDeadLetterQueue = new Queue(context, 'ProcessManifestProcessingDeadLetterQueue', {
       retentionPeriod: Duration.days(14),
     });
 
-    this.processManifestSqsQueue = new Queue(this, 'ProcessManifestProcessingQueue', {
+    const processManifestSqsQueue = new Queue(context, 'ProcessManifestProcessingQueue', {
       visibilityTimeout: Duration.minutes(1),
       deadLetterQueue: {
         queue: processManifestDeadLetterQueue,
@@ -55,16 +79,23 @@ export class EtlStorageStack extends cdk.Stack {
       },
     });
 
-    const processSbomDeadLetterQueue = new Queue(this, 'ProcessSbomProcessingDeadLetterQueue', {
+    const processSbomDeadLetterQueue = new Queue(context, 'ProcessSbomProcessingDeadLetterQueue', {
       retentionPeriod: Duration.days(14),
     });
 
-    this.processSbomSqsQueue = new Queue(this, 'ProcessSbomProcessingQueue', {
+    const processSbomSqsQueue = new Queue(context, 'ProcessSbomProcessingQueue', {
       visibilityTimeout: Duration.minutes(1),
       deadLetterQueue: {
         queue: processSbomDeadLetterQueue,
         maxReceiveCount: 10,
       },
     });
+
+    return {
+      sbomBucket,
+      manifestBucket,
+      processManifestSqsQueue,
+      processSbomSqsQueue,
+    };
   }
 }
