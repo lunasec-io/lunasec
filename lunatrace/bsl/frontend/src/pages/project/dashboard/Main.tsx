9d33f66ed634a86c9f7d6c4b60af63875296ceaf
Forrest <forrest@lunasec.io>
2022-03-30 15:20:46 -0700

    sidebar reacts to url changes
    Former-commit-id: 63e231ad5a23e7c730e4e9f34cacc25d45f51898
    Former-commit-id: 346f1e6534ca193c2e6b8913393937bf8a031a16

--- lunatrace/bsl/frontend/src/pages/project/dashboard/Main.tsx
+++ lunatrace/bsl/frontend/src/pages/project/dashboard/Main.tsx
@@ -26,7 +26,6 @@ interface ProjectDashboardMainProps {
 }
 
 export const ProjectDashboardMain: React.FunctionComponent<ProjectDashboardMainProps> = ({ project }) => {
-  console.log('rendering main dashboard');
   const { project_id } = useParams();
   if (!project_id) {
     return null;
