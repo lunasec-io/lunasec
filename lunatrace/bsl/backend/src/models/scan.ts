b473e9e0f940a506cf5c4d9c3be561c03c2c71ce
breadchris <chris@lunasec.io>
2022-03-30 21:14:31 -0700

    hasura mutation for creating scan and findings

--- lunatrace/bsl/backend/src/models/scan.ts
+++ lunatrace/bsl/backend/src/models/scan.ts
@@ -14,59 +14,78 @@
 import { spawn } from 'child_process';
 import Stream, { Readable } from 'stream';
 
-import { db, pgp } from '../database/db';
-import { Convert, Match as GrypeMatch, GrypeScanReport } from '../types/grype-scan-report';
-import { Finding, Report } from '../types/scan';
+import { hasura } from '../hasura-api';
+import {
+  Findings_Arr_Rel_Insert_Input,
+  Findings_Constraint,
+  Findings_Insert_Input,
+  Findings_Update_Column,
+  Package_Versions_Constraint,
+  Scans_Insert_Input,
+  Vulnerabilities_Constraint,
+  Vulnerability_Packages_Constraint,
+} from '../hasura-api/generated';
+import { Convert, GrypeScanReport, Match } from '../types/grype-scan-report';
 
-export class Scan {
-  static async uploadScan(sbomStream: Readable, buildId: string): Promise<Report> {
-    const rawGrypeReport = await this.runGrypeScan(sbomStream);
-    const typedRawGrypeReport = Convert.toScanReport(rawGrypeReport);
-    const report = this.parseScan(typedRawGrypeReport, buildId);
-    await this.storeReport(report);
-    return report;
-  }
+export async function parseAndUploadScan(sbomStream: Readable, buildId: string): Promise<Scans_Insert_Input> {
+  const rawGrypeReport = await runGrypeScan(sbomStream);
+  const typedRawGrypeReport = Convert.toScanReport(rawGrypeReport);
+  const scan = parseScan(typedRawGrypeReport, buildId);
+  await hasura.InsertScan({
+    scan,
+  });
+  return scan;
+}
 
-  static async runGrypeScan(sbomStream: Readable): Promise<string> {
-    return new Promise((resolve, reject) => {
-      const stdoutStream = new Stream.Writable();
-      // const stderrStream = new Stream.Writeable();
-      const grypeCli = spawn(`lunatrace`, ['--log-to-stderr', 'scan', '--stdin', '--stdout']);
-      grypeCli.on('error', reject);
-      const outputBuffers: Buffer[] = [];
-      grypeCli.stdout.on('data', (chunk) => {
-        outputBuffers.push(Buffer.from(chunk));
-      });
-      grypeCli.stderr.on('data', (errorChunk) => {
-        console.error(errorChunk.toString());
-      });
-      grypeCli.on('close', (code) => {
-        if (code !== 0) {
-          return reject(`Grype exited with non-zero code: ${code}`);
-        }
-        resolve(Buffer.concat(outputBuffers).toString());
-      });
-      sbomStream.on('data', (chunk) => grypeCli.stdin.write(chunk));
-      sbomStream.on('end', () => grypeCli.stdin.end(() => console.log('Finished passing sbom contents to grype')));
-      sbomStream.on('error', reject);
+async function runGrypeScan(sbomStream: Readable): Promise<string> {
+  return new Promise((resolve, reject) => {
+    const stdoutStream = new Stream.Writable();
+    // const stderrStream = new Stream.Writeable();
+    const grypeCli = spawn(`lunatrace`, ['--log-to-stderr', 'scan', '--stdin', '--stdout']);
+    grypeCli.on('error', reject);
+    const outputBuffers: Buffer[] = [];
+    grypeCli.stdout.on('data', (chunk) => {
+      outputBuffers.push(Buffer.from(chunk));
     });
-  }
+    grypeCli.stderr.on('data', (errorChunk) => {
+      console.error(errorChunk.toString());
+    });
+    grypeCli.on('close', (code) => {
+      if (code !== 0) {
+        return reject(`Grype exited with non-zero code: ${code}`);
+      }
+      resolve(Buffer.concat(outputBuffers).toString());
+    });
+    sbomStream.on('data', (chunk) => grypeCli.stdin.write(chunk));
+    sbomStream.on('end', () => grypeCli.stdin.end(() => console.log('Finished passing sbom contents to grype')));
+    sbomStream.on('error', reject);
+  });
+}
 
-  static parseScan(scan: GrypeScanReport, buildId: string): Report {
-    return {
-      findings: this.parseMatches(scan.matches),
-      source_type: scan.source.type,
-      target: scan.source.target,
-      db_date: scan.descriptor.db.built,
-      grype_version: scan.descriptor.version,
-      distro_name: scan.distro.name,
-      distro_version: scan.distro.version,
-      build_id: buildId,
-    };
-  }
+function parseScan(scan: GrypeScanReport, buildId: string): Scans_Insert_Input {
+  return {
+    findings: parseMatches(scan.matches),
+    source_type: scan.source.type,
+    target: scan.source.target,
+    db_date: scan.descriptor.db.built,
+    grype_version: scan.descriptor.version,
+    distro_name: scan.distro.name,
+    distro_version: scan.distro.version,
+    build_id: buildId,
+  };
+}
 
-  static parseMatches(matches: GrypeMatch[]): Finding[] {
-    return matches.map((match): Finding => {
+function parseMatches(matches: Match[]): Findings_Arr_Rel_Insert_Input {
+  return {
+    on_conflict: {
+      constraint: Findings_Constraint.FindingsDedupeSlugBuildIdKey,
+      update_columns: [
+        Findings_Update_Column.Version,
+        Findings_Update_Column.VirtualPath,
+        Findings_Update_Column.Severity,
+      ],
+    },
+    data: matches.map((match): Findings_Insert_Input => {
       const { vulnerability, artifact } = match;
       const details = match.matchDetails[0];
       // slugs
@@ -89,100 +108,31 @@ export class Scan {
         dedupe_slug: pkg_slug + locations.sort().join(':'),
         fix_state: vulnerability.fix?.state || null,
         fix_versions: vulnerability.fix?.versions || null,
-        meta: {
-          vuln_slug,
-          pkg_slug,
-          version_slug,
+        vulnerability: {
+          data: {
+            slug: version_slug,
+          },
+          on_conflict: {
+            constraint: Vulnerabilities_Constraint.VulnerabilitiesSlugKey,
+          },
+        },
+        vulnerability_package: {
+          data: {
+            slug: pkg_slug,
+          },
+          on_conflict: {
+            constraint: Vulnerability_Packages_Constraint.VulnerabilityPackagesSlugKey,
+          },
+        },
+        package_version: {
+          data: {
+            slug: version_slug,
+          },
+          on_conflict: {
+            constraint: Package_Versions_Constraint.PackageVersionsSlugKey,
+          },
         },
       };
-    });
-  }
-
-  static async storeReport(report: Report) {
-    const queryBeginning = pgp.as.format(
-      `
-          BEGIN;
-          WITH this_scan AS (
-            INSERT INTO public.scans(
-                source_type, 
-                target, 
-                db_date,
-                grype_version, 
-                distro_name, 
-                distro_version, 
-                build_id
-                )
-            VALUES(
-                 $<source_type>,
-                 $<target>,
-                 $<db_date>,
-                 $<grype_version>,
-                 $<distro_name>,
-                 $<distro_version>,
-                 $<build_id>
-            ) RETURNING id, build_id
-          )
-          INSERT INTO findings(
-            vulnerability_id,
-            vulnerability_package_id,
-            package_version_id,
-            scan_id,
-            build_id,
-            package_name,
-            version,
-            version_matcher,
-            type,
-            locations,
-            language,
-            purl,
-            virtual_path,
-            matcher,
-            dedupe_slug,
-            severity,
-            fix_state,
-            fix_versions
-          ) VALUES 
-        `,
-      report
-    );
-
-    const findingValueArray = report.findings.map(this.buildFindingInsertQuery);
-    const findingValues = findingValueArray.join(',');
-    const queryEnd = `    ON CONFLICT (dedupe_slug, build_id) DO UPDATE
-                            SET
-                                version = EXCLUDED.version,
-                                virtual_path = EXCLUDED.virtual_path,
-                                severity = EXCLUDED.severity
-                      ; COMMIT;`;
-    const query = queryBeginning + findingValues + queryEnd;
-    await db.none(query);
-  }
-
-  private static buildFindingInsertQuery(finding: Finding) {
-    return pgp.as.format(
-      `
-            (
-            ( SELECT id FROM public.vulnerabilities WHERE slug = $<meta.vuln_slug> ),
-            ( SELECT id FROM public.vulnerability_packages WHERE slug = $<meta.pkg_slug>),
-            ( SELECT id FROM public.package_versions WHERE slug = $<meta.version_slug>),
-            ( SELECT id FROM this_scan ),
-            ( SELECT build_id FROM this_scan ),
-            $<package_name>,
-            $<version>,
-            $<version_matcher>,
-            $<type>,
-            $<locations>,
-            $<language>,
-            $<purl>,
-            $<virtual_path>,
-            $<matcher>,
-            $<dedupe_slug>,
-            $<severity>,
-            $<fix_state>,
-            $<fix_versions>
-          )
-        `,
-      finding
-    );
-  }
+    }),
+  };
 }
