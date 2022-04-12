bbb696c8d91d119413359815d1f4f2d3d8f72247
breadchris <chris@lunasec.io>
2022-04-11 21:58:45 -0700

    add down and up migration for auth refactor

--- lunatrace/bsl/hasura/migrations/lunatrace/1649717845749_create-users-table-use-as-auth-table/up.sql
+++ lunatrace/bsl/hasura/migrations/lunatrace/1649717845749_create-users-table-use-as-auth-table/up.sql
@@ -1,25 +1,22 @@
-CREATE TABLE "public"."users" (
+CREATE TABLE public.users (
     "id" uuid NOT NULL DEFAULT public.gen_random_uuid(),
-    "github_id" text UNIQUE NOT NULL,
+    "github_id" text UNIQUE,
     "github_node_id" text UNIQUE,
     "kratos_id" uuid,
     PRIMARY KEY ("id"),
     FOREIGN KEY ("kratos_id")
-       REFERENCES "public"."identities"("id")
+       REFERENCES public.identities("id")
        ON UPDATE cascade
        ON DELETE cascade
 );
+COMMENT ON TABLE public.users IS E'LunaTrace users, identified by their various auth identifiers (ex. github, kratos, etc.)';
 
-COMMENT ON TABLE "public"."users" IS E'LunaTrace users, identified by their various auth identifiers (ex. github, kratos, etc.)';
+ALTER TABLE public.organization_user DROP CONSTRAINT organization_user_user_id_fkey;
 
-ALTER TABLE "public"."organization_user" DROP CONSTRAINT "organization_user_user_id_fkey",
-  ADD CONSTRAINT "organization_user_user_id_fkey"
-  FOREIGN KEY ("user_id")
-  REFERENCES "public"."users"
-  ("id") ON UPDATE cascade ON DELETE cascade;
+INSERT INTO public.users (kratos_id) SELECT id FROM public.identities;
+UPDATE public.organization_user as o SET user_id = (SELECT id FROM public.users as u WHERE o.user_id = u.kratos_id);
 
-ALTER TABLE "public"."project_access_tokens"
-  ADD CONSTRAINT "project_access_tokens_created_by_user_id_fkey"
-  FOREIGN KEY ("created_by_user_id")
-  REFERENCES "public"."users"
-  ("id") ON UPDATE NO ACTION ON DELETE SET NULL;
+ALTER TABLE public.organization_user ADD CONSTRAINT organization_user_user_id_fkey
+  FOREIGN KEY (user_id)
+  REFERENCES public.users
+  (id) ON UPDATE cascade ON DELETE cascade;
