4c04298d0ef92e899122292b43087e323a0d4f93
breadchris <chris@lunasec.io>
2022-04-11 21:58:45 -0700

    add down and up migration for auth refactor

--- lunatrace/bsl/hasura/migrations/lunatrace/1649717845749_create-users-table-use-as-auth-table/down.sql
+++ lunatrace/bsl/hasura/migrations/lunatrace/1649717845749_create-users-table-use-as-auth-table/down.sql
@@ -1,13 +1,10 @@
-DROP TABLE "public"."users";
+ALTER TABLE public.organization_user DROP CONSTRAINT organization_user_user_id_fkey;
 
-ALTER TABLE "public"."organization_user" DROP CONSTRAINT "organization_user_user_id_fkey",
-    ADD CONSTRAINT "organization_user_user_id_fkey"
-    FOREIGN KEY ("user_id")
-    REFERENCES "public"."identities"
-    ("id") ON UPDATE cascade ON DELETE CASCADE;
+UPDATE public.organization_user as o SET user_id = (SELECT kratos_id FROM public.users as u WHERE o.user_id = u.id);
 
-ALTER TABLE "public"."project_access_tokens"
-    ADD CONSTRAINT "project_access_tokens_created_by_user_id_fkey"
-        FOREIGN KEY ("created_by_user_id")
-    REFERENCES "public"."identities"
-    ("id") ON UPDATE NO ACTION ON DELETE SET NULL;
+ALTER TABLE public.organization_user ADD CONSTRAINT organization_user_user_id_fkey
+    FOREIGN KEY (user_id)
+    REFERENCES public.identities
+    (id) ON UPDATE cascade ON DELETE CASCADE;
+
+DROP TABLE public.users;
