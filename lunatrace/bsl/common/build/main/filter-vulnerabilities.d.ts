d7b01522c63133c5378f8b204501c9cfe6a7f2fd
Forrest <forrest@lunasec.io>
2022-04-12 20:34:47 -0700

    made free edits
    Former-commit-id: 658b91a3ba9c152f9b5af0d4de69a937ad814269
    Former-commit-id: 5cede9e7ab023da92abb96080b71d20f26f8dff4

--- lunatrace/bsl/common/build/main/filter-vulnerabilities.d.ts
+++
@@ -1,9 +0,0 @@
-export interface FindingToIgnore {
-    locations: string[];
-    vulnerability: {
-        ignored_vulnerabilities: Array<{
-            locations: string[];
-        }>;
-    };
-}
-export declare function filterFindingsByIgnored<F extends FindingToIgnore>(findings: F[]): F[];
