ee7ddaa64ec5da26514d353236b4fab494b2dca9
breadchris <chris@lunasec.io>
2022-04-18 14:30:24 -0700

    add migration for cascading an organization removal
    Former-commit-id: c620e9ebccee0fdf0cdf07e0042abe2fa2bc2692
    Former-commit-id: d4cbdd7d82b7f2153ea76e3a2478d8b5283c2a9f

---
+++ lunatrace/bsl/hasura/migrations/lunatrace/1650071188169_cascade-organization-related-information-on-delete/up.sql
@@ -0,0 +1,36 @@
+
+ALTER TABLE "public"."builds" DROP CONSTRAINT "builds_project_id_fkey",
+  ADD CONSTRAINT "builds_project_id_fkey"
+  FOREIGN KEY ("project_id")
+  REFERENCES "public"."projects"
+  ("id") ON UPDATE CASCADE ON DELETE CASCADE;
+
+ALTER TABLE "public"."manifests" DROP CONSTRAINT "manifests_project_id_fkey",
+  ADD CONSTRAINT "manifests_project_id_fkey"
+  FOREIGN KEY ("project_id")
+  REFERENCES "public"."projects"
+  ("id") ON UPDATE CASCADE ON DELETE CASCADE;
+
+ALTER TABLE "public"."manifests" DROP CONSTRAINT "manifests_build_id_fkey",
+  ADD CONSTRAINT "manifests_build_id_fkey"
+  FOREIGN KEY ("build_id")
+  REFERENCES "public"."builds"
+  ("id") ON UPDATE CASCADE ON DELETE CASCADE;
+
+ALTER TABLE "public"."scans" DROP CONSTRAINT "scans_build_id_fkey",
+  ADD CONSTRAINT "scans_build_id_fkey"
+  FOREIGN KEY ("build_id")
+  REFERENCES "public"."builds"
+  ("id") ON UPDATE CASCADE ON DELETE CASCADE;
+
+ALTER TABLE "public"."findings" DROP CONSTRAINT "findings_scan_id_fkey",
+  ADD CONSTRAINT "findings_scan_id_fkey"
+  FOREIGN KEY ("scan_id")
+  REFERENCES "public"."scans"
+  ("id") ON UPDATE CASCADE ON DELETE CASCADE;
+
+ALTER TABLE "public"."findings" DROP CONSTRAINT "findings_build_id_fkey",
+  ADD CONSTRAINT "findings_build_id_fkey"
+  FOREIGN KEY ("build_id")
+  REFERENCES "public"."builds"
+  ("id") ON UPDATE CASCADE ON DELETE CASCADE;
