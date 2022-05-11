49b27af6860bcd6b7022fafdbb25edf64fe81827
Alex Vanderpot <alex@lunasec.io>
2022-05-11 15:40:07 -0700

    rm script

--- mod.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-find . -name '*.go' -print0 | xargs -0 sed -i -e "s|\"lunasec/lunadefend|\"github.com/lunasec-io/lunasec/go|"
-find . -name '*.go' -print0 | xargs -0 sed -i -e "s|\"lunasec/lunatrace/snapshot|\"github.com/lunasec-io/lunasec/lunatrace/cli/pkg/snapshot|"
-find . -name '*.go' -print0 | xargs -0 sed -i -e "s|\"lunasec/lunatrace|\"github.com/lunasec-io/lunasec/lunatrace/cli|"
-
-rm {go,lunatrace/cli,lunatrace/cli/pkg/snapshot}/go.{sum,mod}
