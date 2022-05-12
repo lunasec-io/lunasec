78fd4b59c5eedad62d82d124fc020cc898a1f5d2
Alex Vanderpot <alex@lunasec.io>
2022-05-11 17:56:21 -0700

    Merge all go code into a single module (#611)

--- go/util/misc.go
+++ go/util/misc.go
@@ -19,7 +19,7 @@ import (
 	"os"
 	"regexp"
 
-	"lunasec/lunadefend/constants"
+	"github.com/lunasec-io/lunasec/go/constants"
 )
 
 var uuidPattern = regexp.MustCompile("^" + constants.TokenPrefix + "[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-4[a-fA-F0-9]{3}-[8|9|aA|bB][a-fA-F0-9]{3}-[a-fA-F0-9]{12}$")
