7525ddd689462666d078656026c2ab37157b8c41
Forrest <forrest@lunasec.io>
2022-05-13 18:27:37 -0700

    PR feedback disabled e2e should work

--- lunatrace/bsl/hasura/migrations/lunatrace/1652400983913_add-pr-comment-disable/down.sql
+++ lunatrace/bsl/hasura/migrations/lunatrace/1652400983913_add-pr-comment-disable/down.sql
@@ -1,5 +1,5 @@
-DROP TRIGGER add_project_settings;
-DROP TRIGGER add_organization_settings;
+DROP TRIGGER add_project_settings ON public.projects;
+DROP TRIGGER add_organization_settings ON public.organizations;
 
 ALTER TABLE public.projects DROP CONSTRAINT project_settings_fkey;
 ALTER TABLE public.organizations DROP CONSTRAINT organization_settings_fkey;
