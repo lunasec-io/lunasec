022f8ed7d0b09f28fb94a184180b9919174dbb43
Forrest <forrest@lunasec.io>
2022-05-17 15:49:10 -0700

    switched triggers to DEFAULT, leave flags null until set, false always default

--- lunatrace/bsl/hasura/migrations/lunatrace/1652740128856_package_indexes/down.sql
+++ lunatrace/bsl/hasura/migrations/lunatrace/1652740128856_package_indexes/down.sql
@@ -1,4 +1,3 @@
-DROP INDEX "package"."release_license_release_id_idx"
+DROP INDEX "package"."release_license_release_id_idx";
 
-alter table package.release_dependency
-    drop column is_dev;
+alter table package.release_dependency drop column is_dev;
