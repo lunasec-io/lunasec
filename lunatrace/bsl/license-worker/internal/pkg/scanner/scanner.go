e1a7a5a7539558c95d9d83c119924f872e53cee6
Alex Vanderpot <alex@lunasec.io>
2022-05-19 16:56:56 -0700

    scan
    Former-commit-id: b78cf4ae51ff7611661999cf36fa65222e461556
    Former-commit-id: 095b36026c8ab46f41605dd8e0452ab89c76d8a4

--- lunatrace/bsl/license-worker/internal/pkg/scanner/scanner.go
+++ lunatrace/bsl/license-worker/internal/pkg/scanner/scanner.go
@@ -1,6 +1,6 @@
 // Copyright by LunaSec (owned by Refinery Labs, Inc)
 //
-// Licensed under the Business Source License v1.1 
+// Licensed under the Business Source License v1.1
 // (the "License"); you may not use this file except in compliance with the
 // License. You may obtain a copy of the License at
 //
@@ -13,5 +13,5 @@ package scanner
 
 type Scanner interface {
 	// Scan scans the byte slice F for licenses and returns them as a list of strings.
-	Scan(f []byte) []string
+	Scan(f []byte) ([]string, error)
 }
