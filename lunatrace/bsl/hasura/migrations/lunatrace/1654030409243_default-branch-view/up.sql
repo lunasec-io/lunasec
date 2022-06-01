91cc53a704a5c6f70a5fd08eccecb63f1598d5e4
Forrest <forrest@lunasec.io>
2022-05-31 17:12:16 -0700

    added view

---
+++ lunatrace/bsl/hasura/migrations/lunatrace/1654030409243_default-branch-view/up.sql
@@ -0,0 +1,11 @@
+-- Creates a view that only shows the builds on default branch
+
+CREATE VIEW public.default_branch_builds AS
+SELECT *
+FROM public.builds AS build
+WHERE build.git_branch = (SELECT default_branch
+                          FROM public.projects project
+                                   INNER JOIN public.github_repositories
+                                              ON project.id = public.github_repositories.project_id
+                          WHERE project.id = build.project_id)
+;
