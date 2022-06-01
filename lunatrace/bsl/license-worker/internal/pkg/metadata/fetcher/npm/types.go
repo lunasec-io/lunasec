10bf89e102e09a607a00553f036568c8456e3ab8
Alex Vanderpot <alex@lunasec.io>
2022-06-01 12:52:16 -0700

    package upsert

---
+++ lunatrace/bsl/license-worker/internal/pkg/metadata/fetcher/npm/types.go
@@ -0,0 +1,140 @@
+// Copyright by LunaSec (owned by Refinery Labs, Inc)
+//
+// Licensed under the Business Source License v1.1
+// (the "License"); you may not use this file except in compliance with the
+// License. You may obtain a copy of the License at
+//
+// https://github.com/lunasec-io/lunasec/blob/master/licenses/BSL-LunaTrace.txt
+//
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// Copyright 2022 by LunaSec (owned by Refinery Labs, Inc)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+package npm
+
+import "encoding/json"
+
+type NpmPackageMetadata struct {
+	Attachments Attachments `json:"_attachments"`
+	ID          string      `json:"_id"`
+	Rev         string      `json:"_rev"`
+	// either Author or string
+	Author         json.RawMessage    `json:"author"`
+	Description    string             `json:"description"`
+	DistTags       DistTags           `json:"dist-tags"`
+	License        json.RawMessage    `json:"license"`
+	Maintainers    []Author           `json:"maintainers"`
+	Name           string             `json:"name"`
+	Readme         string             `json:"readme"`
+	ReadmeFilename string             `json:"readmeFilename"`
+	Time           Time               `json:"time"`
+	Versions       map[string]Version `json:"versions"`
+}
+
+type NpmPackageMetadataWithRawVersion struct {
+	Attachments Attachments `json:"_attachments"`
+	ID          string      `json:"_id"`
+	Rev         string      `json:"_rev"`
+	// either Author or string
+	Author         json.RawMessage            `json:"author"`
+	Description    string                     `json:"description"`
+	DistTags       DistTags                   `json:"dist-tags"`
+	License        json.RawMessage            `json:"license"`
+	Maintainers    []Author                   `json:"maintainers"`
+	Name           string                     `json:"name"`
+	Readme         string                     `json:"readme"`
+	ReadmeFilename string                     `json:"readmeFilename"`
+	Time           Time                       `json:"time"`
+	VersionsRaw    map[string]json.RawMessage `json:"versions"`
+}
+
+type Attachments struct {
+}
+
+type Author struct {
+	Email string `json:"email"`
+	Name  string `json:"name"`
+}
+
+type DistTags struct {
+	Latest string `json:"latest"`
+}
+
+// either string or
+// {"_id":"dsr-rollback-parry-virid-vespa-kiley","name":"dsr-rollback-parry-virid-vespa-kiley","time":{"created":"2021-12-14T08:24:16.841Z","1.0.0":"2021-12-14T08:24:17.035Z","modified":"2021-12-14T08:24:37.596Z","unpublished":{"time":"2021-12-14T08:24:37.596Z","versions":["1.0.0"]}}}
+type Time map[string]json.RawMessage
+
+//type Time struct {
+//	The100   string `json:"1.0.0"`
+//	Created  string `json:"created"`
+//	Modified string `json:"modified"`
+//}
+
+type Version struct {
+	Name            string                     `json:"name"`
+	Version         string                     `json:"version"`
+	Description     string                     `json:"description"`
+	Main            json.RawMessage            `json:"main"`
+	Files           []string                   `json:"files"`
+	Dependencies    map[string]string          `json:"dependencies"`
+	DevDependencies map[string]string          `json:"devDependencies"`
+	Scripts         map[string]json.RawMessage `json:"scripts"`
+	Repository      json.RawMessage            `json:"repository"`
+	Standard        Standard                   `json:"standard"`
+	Keywords        json.RawMessage            `json:"keywords"`
+	// Can be either Author or set to an empty string
+	Author       json.RawMessage `json:"author"`
+	Contributors json.RawMessage `json:"contributors"`
+	License      json.RawMessage `json:"license"`
+	Engine       json.RawMessage `json:"engine"`
+	GitHead      string          `json:"gitHead"`
+	Bugs         json.RawMessage `json:"bugs"`
+	Homepage     json.RawMessage `json:"homepage"`
+	ID           string          `json:"_id"`
+	Shasum       string          `json:"_shasum"`
+	From         string          `json:"_from"`
+	NpmVersion   string          `json:"_npmVersion"`
+	NodeVersion  string          `json:"_nodeVersion"`
+	NpmUser      Author          `json:"_npmUser"`
+	Dist         Dist            `json:"dist"`
+	Maintainers  []Author        `json:"maintainers"`
+	Directories  json.RawMessage `json:"directories"`
+}
+
+type Engine struct {
+	Node string `json:"node"`
+}
+
+type Bugs struct {
+	URL string `json:"url"`
+}
+
+type Repository struct {
+	Type string `json:"type"`
+	URL  string `json:"url"`
+}
+
+type Standard struct {
+	Ignore  json.RawMessage `json:"ignore"`
+	Globals []string        `json:"globals"`
+}
+
+type Dist struct {
+	Shasum  string `json:"shasum"`
+	Tarball string `json:"tarball"`
+}
+
+type Scripts map[string]string
---
+++ lunatrace/bsl/license-worker/internal/pkg/metadata/mapper/upsert.go
@@ -0,0 +1,104 @@
+// Copyright by LunaSec (owned by Refinery Labs, Inc)
+//
+// Licensed under the Business Source License v1.1
+// (the "License"); you may not use this file except in compliance with the
+// License. You may obtain a copy of the License at
+//
+// https://github.com/lunasec-io/lunasec/blob/master/licenses/BSL-LunaTrace.txt
+//
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+package mapper
+
+import (
+	"github.com/lunasec-io/lunasec/lunatrace/bsl/license-worker/internal/pkg/metadata/fetcher"
+	"github.com/lunasec-io/lunasec/lunatrace/cli/gql"
+	"github.com/lunasec-io/lunasec/lunatrace/cli/gql/types"
+)
+
+func Map(p *fetcher.PackageMetadata) (*gql.Package_insert_input, error) {
+	r := &gql.Package_insert_input{
+		Custom_registry: p.Registry,
+		Description:     p.Description,
+		Name:            p.Name,
+		Package_maintainers: &gql.Package_package_maintainer_arr_rel_insert_input{
+			Data:        mapMaintainers(p.Maintainers),
+			On_conflict: gql.PackageMaintainerOnConflict,
+		},
+		Package_manager: types.NPM,
+		Releases: &gql.Package_release_arr_rel_insert_input{
+			Data:        mapReleases(p.Releases),
+			On_conflict: gql.ReleaseOnConflict,
+		},
+	}
+	return r, nil
+}
+
+func mapReleases(r []fetcher.Release) []*gql.Package_release_insert_input {
+	m := make([]*gql.Package_release_insert_input, len(r))
+	for i, rl := range r {
+		m[i] = &gql.Package_release_insert_input{
+			Publishing_maintainer: mapMaintainer(rl.PublishingMaintainer),
+
+			Release_time:      rl.ReleaseTime,
+			Blob_hash:         rl.BlobHash,
+			Upstream_blob_url: rl.UpstreamBlobUrl,
+			Upstream_data:     rl.UpstreamData,
+			Version:           rl.Version,
+
+			Mirrored_blob_url: "",
+
+			Release_dependencies: &gql.Package_release_dependency_arr_rel_insert_input{
+				Data:        mapDependencies(rl.Dependencies),
+				On_conflict: gql.PackageReleaseDependencyOnConflict,
+			},
+		}
+
+	}
+
+	return m
+}
+
+func mapDependencies(ds []fetcher.Dependency) []*gql.Package_release_dependency_insert_input {
+	m := make([]*gql.Package_release_dependency_insert_input, len(ds))
+	for i, dep := range ds {
+		m[i] = &gql.Package_release_dependency_insert_input{
+			// create a stub entry for packages which are not yet analyzed.
+			Dependency_package: &gql.Package_obj_rel_insert_input{
+				Data: &gql.Package_insert_input{
+					Name:            dep.Name,
+					Package_manager: types.NPM,
+				},
+				On_conflict: gql.PackageOnConflict,
+			},
+			// todo we dont have resolved versions maybe so idk
+			Dependency_release:    nil,
+			Package_name:          dep.Name,
+			Package_version_query: dep.Version,
+		}
+	}
+	return m
+}
+
+func mapMaintainers(p []fetcher.Maintainer) []*gql.Package_package_maintainer_insert_input {
+	m := make([]*gql.Package_package_maintainer_insert_input, len(p))
+	for i, pm := range p {
+		m[i] = &gql.Package_package_maintainer_insert_input{
+			Maintainer: mapMaintainer(pm),
+		}
+
+	}
+	return m
+}
+
+func mapMaintainer(pm fetcher.Maintainer) *gql.Package_maintainer_obj_rel_insert_input {
+	return &gql.Package_maintainer_obj_rel_insert_input{
+		Data: &gql.Package_maintainer_insert_input{
+			Email:           pm.Email,
+			Name:            pm.Name,
+			Package_manager: types.NPM,
+		},
+		On_conflict: gql.MaintainerOnConflict,
+	}
+}
