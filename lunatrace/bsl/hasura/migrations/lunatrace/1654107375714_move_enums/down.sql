c0af69310da152fad642b5b2ea4be55c02ecb5b2
Alex Vanderpot <alex@lunasec.io>
2022-06-02 15:10:45 -0700

    code style settings

--- lunatrace/bsl/hasura/migrations/lunatrace/1654107375714_move_enums/down.sql
+++ lunatrace/bsl/hasura/migrations/lunatrace/1654107375714_move_enums/down.sql
@@ -1,52 +1,52 @@
-create type package.license_source as enum ('manual', 'scan_repo', 'scan_binary', 'api_npm');
+CREATE TYPE package.license_source AS ENUM ('manual', 'scan_repo', 'scan_binary', 'api_npm');
 
-create type package.package_manager as enum ('npm');
+CREATE TYPE package.package_manager AS ENUM ('npm');
 
-alter table package.release_license
-    alter column source type package.license_source using source::text::package.license_source;
+ALTER TABLE package.release_license
+    ALTER COLUMN source TYPE package.license_source USING source::TEXT::package.license_source;
 
-alter table package.package
-    alter column package_manager type package.package_manager using package_manager::text::package.package_manager;
+ALTER TABLE package.package
+    ALTER COLUMN package_manager TYPE package.package_manager USING package_manager::TEXT::package.package_manager;
 
-alter table package.maintainer
-    alter column package_manager type package.package_manager using package_manager::text::package.package_manager;
+ALTER TABLE package.maintainer
+    ALTER COLUMN package_manager TYPE package.package_manager USING package_manager::TEXT::package.package_manager;
 
-drop type public.license_source;
+DROP TYPE public.license_source;
 
-drop type public.package_manager;
+DROP TYPE public.package_manager;
 
-CREATE UNIQUE INDEX ON "package"."maintainer" ("package_manager", "email");
+CREATE UNIQUE INDEX ON package.maintainer (package_manager, email);
 ALTER TABLE package.maintainer
     DROP CONSTRAINT maintainer_package_manager_email_idx;
 
-CREATE UNIQUE INDEX ON "package"."package_maintainer" ("package_id", "maintainer_id");
+CREATE UNIQUE INDEX ON package.package_maintainer (package_id, maintainer_id);
 ALTER TABLE package.package_maintainer
     DROP CONSTRAINT package_maintainer_package_id_maintainer_id_idx;
 
-CREATE UNIQUE INDEX ON "package"."package" ("package_manager", "custom_registry", "name");
+CREATE UNIQUE INDEX ON package.package (package_manager, custom_registry, name);
 ALTER TABLE package.package
     DROP CONSTRAINT package_package_manager_custom_registry_name_idx;
 
-CREATE UNIQUE INDEX ON "package"."release" ("package_id", "version");
+CREATE UNIQUE INDEX ON package.release (package_id, version);
 ALTER TABLE package.release
     DROP CONSTRAINT release_package_id_version_idx;
 
-CREATE UNIQUE INDEX ON "package"."release_dependency" ("release_id", "package_name", "package_version_query");
+CREATE UNIQUE INDEX ON package.release_dependency (release_id, package_name, package_version_query);
 ALTER TABLE package.release_dependency
     DROP CONSTRAINT release_dependency_release_id_package_name_package_version__idx;
 
-CREATE UNIQUE INDEX ON "package"."license" ("name");
+CREATE UNIQUE INDEX ON package.license (name);
 ALTER TABLE package.license
     DROP CONSTRAINT license_name_idx;
 
-alter table package.package
-    drop column fetched_time;
+ALTER TABLE package.package
+    DROP COLUMN fetched_time;
 
-alter table package.release
-    drop column fetched_time;
+ALTER TABLE package.release
+    DROP COLUMN fetched_time;
 
-alter table package.package
-    alter column custom_registry drop not null;
+ALTER TABLE package.package
+    ALTER COLUMN custom_registry DROP NOT NULL;
 
-alter table package.package
-    alter column custom_registry drop default;
+ALTER TABLE package.package
+    ALTER COLUMN custom_registry DROP DEFAULT;
--- lunatrace/bsl/hasura/migrations/lunatrace/1654107375714_move_enums/up.sql
+++ lunatrace/bsl/hasura/migrations/lunatrace/1654107375714_move_enums/up.sql
@@ -1,52 +1,52 @@
-create type public.license_source as enum ('manual', 'scan_repo', 'scan_binary', 'api_npm');
+CREATE TYPE public.license_source AS ENUM ('manual', 'scan_repo', 'scan_binary', 'api_npm');
 
-create type public.package_manager as enum ('npm');
+CREATE TYPE public.package_manager AS ENUM ('npm');
 
-alter table package.release_license
-    alter column source type public.license_source using source::text::public.license_source;
+ALTER TABLE package.release_license
+    ALTER COLUMN source TYPE public.license_source USING source::TEXT::public.license_source;
 
-alter table package.package
-    alter column package_manager type public.package_manager using package_manager::text::public.package_manager;
+ALTER TABLE package.package
+    ALTER COLUMN package_manager TYPE public.package_manager USING package_manager::TEXT::public.package_manager;
 
-alter table package.maintainer
-    alter column package_manager type public.package_manager using package_manager::text::public.package_manager;
+ALTER TABLE package.maintainer
+    ALTER COLUMN package_manager TYPE public.package_manager USING package_manager::TEXT::public.package_manager;
 
-drop type package.license_source;
+DROP TYPE package.license_source;
 
-drop type package.package_manager;
+DROP TYPE package.package_manager;
 
-drop index if exists package.release_dependency_release_id_package_name_package_version__idx;
+DROP INDEX IF EXISTS package.release_dependency_release_id_package_name_package_version__idx;
 ALTER TABLE package.release_dependency
     ADD CONSTRAINT release_dependency_release_id_package_name_package_version__idx UNIQUE (release_id, package_name, package_version_query);
 
-drop index if exists package.release_package_id_version_idx;
+DROP INDEX IF EXISTS package.release_package_id_version_idx;
 ALTER TABLE package.release
     ADD CONSTRAINT release_package_id_version_idx UNIQUE (package_id, version);
 
-drop index if exists package.package_maintainer_package_id_maintainer_id_idx;
+DROP INDEX IF EXISTS package.package_maintainer_package_id_maintainer_id_idx;
 ALTER TABLE package.package_maintainer
     ADD CONSTRAINT package_maintainer_package_id_maintainer_id_idx UNIQUE (package_id, maintainer_id);
 
-drop index if exists package.package_package_manager_custom_registry_name_idx;
+DROP INDEX IF EXISTS package.package_package_manager_custom_registry_name_idx;
 ALTER TABLE package.package
     ADD CONSTRAINT package_package_manager_custom_registry_name_idx UNIQUE (package_manager, custom_registry, name);
 
-drop index if exists package.maintainer_package_manager_email_idx;
+DROP INDEX IF EXISTS package.maintainer_package_manager_email_idx;
 ALTER TABLE package.maintainer
     ADD CONSTRAINT maintainer_package_manager_email_idx UNIQUE (package_manager, email);
 
-drop index if exists package.license_name_idx;
+DROP INDEX IF EXISTS package.license_name_idx;
 ALTER TABLE package.license
     ADD CONSTRAINT license_name_idx UNIQUE (name);
 
-alter table package.package
-    add fetched_time timestamptz;
+ALTER TABLE package.package
+    ADD fetched_time timestamptz;
 
-alter table package.release
-    add fetched_time timestamptz;
+ALTER TABLE package.release
+    ADD fetched_time timestamptz;
 
-alter table package.package
-    alter column custom_registry set not null;
+ALTER TABLE package.package
+    ALTER COLUMN custom_registry SET NOT NULL;
 
-alter table package.package
-    alter column custom_registry set default '';
+ALTER TABLE package.package
+    ALTER COLUMN custom_registry SET DEFAULT '';
