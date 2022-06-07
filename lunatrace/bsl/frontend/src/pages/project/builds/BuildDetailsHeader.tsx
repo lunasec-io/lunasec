5e370de5ca48fa1213a1d051eff2699f0ffa1d76
Forrest <forrest@lunasec.io>
2022-06-06 17:45:46 -0700

    side by side view working for findings
    Former-commit-id: 76b464658b4e4959a7512944d99a990097f174fa
    Former-commit-id: 3338f2373a8bd60a465e7c544ed51ac61934ffcb

---
+++ lunatrace/bsl/frontend/src/pages/project/builds/BuildDetailsHeader.tsx
@@ -0,0 +1,116 @@
+/*
+ * Copyright by LunaSec (owned by Refinery Labs, Inc)
+ *
+ * Licensed under the Business Source License v1.1
+ * (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ * https://github.com/lunasec-io/lunasec/blob/master/licenses/BSL-LunaTrace.txt
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import { ArrowLeft } from 'react-feather';
+import { NavLink } from 'react-router-dom';
+
+import api from '../../../api';
+import { ConditionallyRender } from '../../../components/utils/ConditionallyRender';
+import { branchLink, branchName, commitLink } from '../../../utils/build-display-helpers';
+import { prettyDate } from '../../../utils/pretty-date';
+import { capitalizeFirstLetter } from '../../../utils/string-utils';
+import { BuildInfo } from '../types';
+
+import { SourceIcon } from './SourceIcon';
+import { BuildDetailInfo } from './types';
+
+export const BuildDetailsHeader: React.FC<{ build: BuildDetailInfo }> = ({ build }) => {
+  const [getSbomUrl, sbomUrlResult, lastPromiseInfo] = api.useLazyGetSbomUrlQuery();
+
+  const firstScan = build.scans[0];
+
+  const lastScannedDate = firstScan ? prettyDate(new Date(firstScan.created_at as string)) : 'Never';
+  const uploadDate = prettyDate(new Date(build.created_at as string));
+
+  // const githubUrl = gitUrlToLink(build);
+
+  const branch = branchName(build);
+  const branchUrl = branchLink(build);
+  const commitUrl = commitLink(build);
+
+  return (
+    <Container>
+      <Row>
+        <Col xs="3">
+          <NavLink to="..">
+            <ArrowLeft />
+            All Snapshots
+          </NavLink>
+        </Col>
+        <Col xs="6" style={{ textAlign: 'center' }}>
+          <h1>Snapshot #{build.build_number}</h1>
+          <span>{build.project?.name}</span>
+          <h5>{uploadDate}</h5>
+        </Col>
+      </Row>
+      <Row>
+        <Col xs="12" sm={{ order: 'last', span: 5, offset: 4 }}>
+          <h6 style={{ textAlign: 'right' }}>
+            <span className="darker"> Last scanned:</span> {lastScannedDate}
+          </h6>
+          <h6 style={{ textAlign: 'right' }}>
+            <span className="darker">
+              Scanned {build.scans_aggregate.aggregate?.count} time
+              {build.scans_aggregate.aggregate?.count !== 1 ? 's' : ''}
+            </span>
+          </h6>
+          <h6 style={{ textAlign: 'right' }}>
+            <span className="darker">
+              <a
+                href={'#'}
+                onClick={async (e) => {
+                  const url = (await getSbomUrl({ build_id: build.id }))?.data?.builds_by_pk?.s3_url_signed;
+                  if (url) {
+                    window.location.href = url;
+                  }
+                }}
+              >
+                Download SBOM
+              </a>
+            </span>
+          </h6>
+        </Col>
+        <Col xs="12" sm="3">
+          <div className="build-git-info">
+            <h6>
+              <span className="darker">Source: </span>
+              <SourceIcon source_type={build.source_type} className="mb-1" />{' '}
+              <span className="text-capitalize">{build.source_type}</span>{' '}
+            </h6>
+            <ConditionallyRender if={branchUrl}>
+              <h6>
+                <span className="darker">Branch: </span>{' '}
+                <a target="_blank" rel="noopener noreferrer" href={branchUrl || ''}>
+                  {branch}
+                </a>
+              </h6>
+            </ConditionallyRender>
+            <ConditionallyRender if={commitUrl}>
+              <h6>
+                <span className="darker">Commit: </span>{' '}
+                <a target="_blank" rel="noopener noreferrer" href={commitUrl || ''}>
+                  {build.git_hash?.substring(0, 8)}...
+                </a>
+              </h6>
+            </ConditionallyRender>
+            <h6>
+              <span className="darker">{capitalizeFirstLetter(firstScan.source_type)}:</span> {firstScan.target}
+            </h6>
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
