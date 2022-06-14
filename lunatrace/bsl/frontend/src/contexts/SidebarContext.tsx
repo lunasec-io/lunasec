6bdeba8e2da8ef81030b8a8363a595a6eb461b85
breadchris <chris@lunasec.io>
2022-06-14 13:10:34 -0700

    moved all sidebar queries into context to make sure every query is performed with user's id

--- lunatrace/bsl/frontend/src/contexts/SidebarContext.tsx
+++ lunatrace/bsl/frontend/src/contexts/SidebarContext.tsx
@@ -11,12 +11,27 @@
  * limitations under the License.
  *
  */
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
+import api from '../api';
+import { GetSidebarInfoQuery } from '../api/generated';
 import { SIDEBAR_BEHAVIOR, SIDEBAR_POSITION } from '../constants';
+import useAppSelector from '../hooks/useAppSelector';
 import useSettingsState from '../hooks/useSettingsState';
+import { selectKratosId } from '../store/slices/authentication';
 
-const initialState = {
+interface SidebarContextType {
+  isOpen: boolean;
+  position: string;
+  behavior: string;
+  setIsOpen: (_a: boolean) => void;
+  setPosition: (_a: string) => void;
+  setBehavior: (_a: string) => void;
+  sidebarData: GetSidebarInfoQuery | undefined;
+  sidebarIsLoaded: boolean;
+}
+
+const initialState: SidebarContextType = {
   isOpen: false,
   position: SIDEBAR_POSITION.LEFT,
   behavior: SIDEBAR_BEHAVIOR.STICKY,
@@ -29,6 +44,8 @@ const initialState = {
   setBehavior: (_a: string) => {
     return;
   },
+  sidebarData: undefined,
+  sidebarIsLoaded: false,
 };
 
 const SidebarContext = React.createContext(initialState);
@@ -37,6 +54,18 @@ function SidebarProvider({ children }: { children: React.ReactNode }) {
   const [isOpen, setIsOpen] = useState(window.innerWidth > 992);
   const [position, setPosition] = useSettingsState('sidebarPosition', SIDEBAR_POSITION.LEFT);
   const [behavior, setBehavior] = useSettingsState('sidebarBehavior', SIDEBAR_BEHAVIOR.STICKY);
+
+  const kratosId = useAppSelector(selectKratosId);
+  const [trigger, result, lastPromiseInfo] = api.useLazyGetSidebarInfoQuery();
+  const { data, isLoading } = result;
+
+  useEffect(() => {
+    if (kratosId) {
+      void trigger({
+        kratos_id: kratosId,
+      });
+    }
+  }, [kratosId]);
 
   return (
     <SidebarContext.Provider
@@ -47,6 +76,8 @@ function SidebarProvider({ children }: { children: React.ReactNode }) {
         setPosition,
         behavior,
         setBehavior,
+        sidebarData: data,
+        sidebarIsLoaded: !isLoading && data !== undefined,
       }}
     >
       {children}
