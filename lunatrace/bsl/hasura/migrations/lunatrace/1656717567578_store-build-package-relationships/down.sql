c04088035a07a5ab72be6c2339e3efa636bd177b
Forrest <forrest@lunasec.io>
2022-07-05 17:38:12 -0700

    new build_dependency_relationship schema and permissions

--- lunatrace/bsl/hasura/migrations/lunatrace/1656717567578_store-build-package-relationships/down.sql
+++ lunatrace/bsl/hasura/migrations/lunatrace/1656717567578_store-build-package-relationships/down.sql
@@ -1,4 +1,5 @@
-DROP TABLE "build_dependency" CASCADE;
 DROP TABLE "build_dependency_relationship" CASCADE;
 
-ALTER TABLE package.package DROP COLUMN synced;
+ALTER TABLE package.package DROP COLUMN last_failed_fetch;
+ALTER TABLE package.package DROP COLUMN last_successful_fetch;
+ALTER TABLE package.package ADD COLUMN fetched_time timestamptz;
