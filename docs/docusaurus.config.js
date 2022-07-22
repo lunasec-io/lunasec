88e6e2088ae47a1103cb558e1fb3e914cb171613
Alex Vanderpot <alex@lunasec.io>
2022-07-22 15:04:01 -0700

    Update docs CTA to point to mailing list signup (#793)
    ship it
    
    Former-commit-id: c6381350aac957624a8a35f121aa4c83bea022fc
    Former-commit-id: f19c23ef97ccf22142076f5b455c3a5289c07176

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -157,9 +157,9 @@ module.exports = {
       //... other Algolia params
     },
     announcementBar: {
-      id: 'star_us',
+      id: 'mailing_list',
       content:
-        '<a href="https://join.slack.com/t/lunaseccommunity/shared_invite/zt-1a8cdfqdg-LbgCYuxzQl7iyZzZ4H4Idw" target="_blank" rel="noopener" style="font-size: 15px; text-decoration: none">Click here to join our Slack and chat with our security team.</a>',
+        '<a href="/docs/pages/mailing-list" style="font-size: 15px; text-decoration: none">Subscribe to our mailing list to get the latest security news and product updates.</a>',
       backgroundColor: '#9ec6ef',
       textColor: '#091E42',
       isCloseable: true,
