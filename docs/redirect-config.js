8993bd61e48893a1cff2fb966d8b7a6bdb88979a
Alex Vanderpot <alex@lunasec.io>
2022-07-22 16:58:00 -0700

    rewrite mailchimp form

--- docs/redirect-config.js
+++ docs/redirect-config.js
@@ -1,17 +1,4 @@
 /*
- * Copyright by LunaSec (owned by Refinery Labs, Inc)
- *
- * Licensed under the Creative Commons Attribution-ShareAlike 4.0 International
- * (the "License"); you may not use this file except in compliance with the
- * License. You may obtain a copy of the License at
- *
- * https://creativecommons.org/licenses/by-sa/4.0/legalcode
- *
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
-/*
  * Copyright 2021 by LunaSec (owned by Refinery Labs, Inc)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -347,10 +334,6 @@ const redirectConfig = {
     {
       from: '/pages/react-sdk/interfaces/SecureUploadProps/',
       to: '/pages/lunadefend/react-sdk/interfaces/SecureUploadProps/'
-    },
-    {
-      from: '/pages/tokenizer-api-spec/',
-      to: '/pages/lunadefend/tokenizer-api-spec/'
     },
     {
       from: '/pages/tokenizer-sdk/',
