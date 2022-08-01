9b768e044f3f12799b86c6457e137080e5babd8f
Forrest <forrest@lunasec.io>
2022-08-01 16:48:44 -0700

    update ts node
    Former-commit-id: e35943152a03093f3cc3ed48c9a0023ede0510b8
    Former-commit-id: 731bb80e95a615ece418a8aa480e660acffa4865

--- lunatrace/bsl/backend/src/manual-scripts/insert-tree.ts
+++ lunatrace/bsl/backend/src/manual-scripts/insert-tree.ts
@@ -1,11 +1,23 @@
-import * as os from 'os';
+/*
+ * Copyright by LunaSec (owned by Refinery Labs, Inc)
+ *
+ * Licensed under the Business Source License v1.1
+ * (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ * https://github.com/lunasec-io/lunasec/blob/master/licenses/BSL-LunaTrace.txt
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 import path from 'path';
 
 import { snapshotPinnedDependencies } from '../snapshot/node-package-tree';
 
 // This is a one-off script for manual testing purposes
 // TODO: change this to whatever directory you want to manually load a tree from
-const fixturePath = path.resolve(os.homedir(), 'tmp/beer-me');
+const fixturePath = path.resolve(__dirname, '../fixtures/manifests/normal-npm-repo');
 console.log('loading from ', fixturePath);
 // again change the buildId to whatever you want
-snapshotPinnedDependencies('736de7fc-2df3-4438-af90-b3596f65dd8d', fixturePath);
+void snapshotPinnedDependencies('736de7fc-2df3-4438-af90-b3596f65dd8d', fixturePath);
