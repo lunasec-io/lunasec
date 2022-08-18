4c5383237e106fd393e2256472cfe1419d2f08d1
Alex Vanderpot <alex@lunasec.io>
2022-08-18 16:49:36 -0700

    stage 1 works

---
+++ lunatrace/bsl/hasura/migrations/lunatrace/1660866229865_merkel_tree_stage_1/down.sql
@@ -0,0 +1,24 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- COMMENT ON COLUMN build_dependency_relationship.id IS 'merkle tree hash of dependency relationship and its transitive dependencies. not a random UUID.';
+--
+-- ALTER TABLE build_dependency_relationship
+--     ALTER COLUMN id DROP DEFAULT;
+--
+-- CREATE TABLE build_dependency
+-- (
+--     build_id                         uuid
+--         REFERENCES builds (id),
+--     release_id                       uuid
+--         REFERENCES package.release (id),
+--     build_dependency_relationship_id uuid,
+--     range                            TEXT NOT NULL,
+--     labels                           jsonb,
+--     project_path                     TEXT NOT NULL
+-- );
+--
+-- COMMENT ON TABLE build_dependency IS 'direct dependencies of builds with pointers to their location in the merkel tree table';
+--
+-- COMMENT ON COLUMN build_dependency.build_dependency_relationship_id IS 'pointer to merkel tree of transitive dependencies';
+--
+-- CREATE INDEX ON build_dependency (build_id);
