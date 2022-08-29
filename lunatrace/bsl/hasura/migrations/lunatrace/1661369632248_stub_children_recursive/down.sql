c7270e2d388f97707cccb491717ee23ba8a4e7a6
Alex Vanderpot <alex@lunasec.io>
2022-08-29 15:03:27 -0700

    rm stub
    Former-commit-id: a10cb443317f3efc1524d49f079039081d05ff5d
    Former-commit-id: fc18391cc714e219891c877a2717c4882668305c

--- lunatrace/bsl/hasura/migrations/lunatrace/1661369632248_stub_children_recursive/down.sql
+++
@@ -1,6 +0,0 @@
--- Could not auto-generate a down migration.
--- Please write an appropriate down migration for the SQL below:
--- CREATE OR REPLACE FUNCTION manifest_dependency_node_children_recursive(manifest_dependency_node_in manifest_dependency_node)
---     RETURNS SETOF manifest_dependency_node AS $$
---     SELECT manifest_dependency_node_in;
--- $$ LANGUAGE sql STABLE;
