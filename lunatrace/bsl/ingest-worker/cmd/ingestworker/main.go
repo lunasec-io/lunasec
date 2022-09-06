cbda4b148ca5cbb648cefa67316c9bf566b13829
breadchris <chris@lunasec.io>
2022-09-06 12:12:20 -0700

    vulnerability cli can clone advisory database

--- lunatrace/bsl/ingest-worker/cmd/ingestworker/main.go
+++ lunatrace/bsl/ingest-worker/cmd/ingestworker/main.go
@@ -1,6 +1,6 @@
 // Copyright by LunaSec (owned by Refinery Labs, Inc)
 //
-// Licensed under the Business Source License v1.1 
+// Licensed under the Business Source License v1.1
 // (the "License"); you may not use this file except in compliance with the
 // License. You may obtain a copy of the License at
 //
@@ -12,18 +12,66 @@
 package main
 
 import (
+	ingest "github.com/lunasec-io/lunasec/lunatrace/bsl/ingest-worker/cmd/ingestworker/package-injest"
+	"github.com/rs/zerolog/log"
+	"net/http"
+
+	"github.com/lunasec-io/lunasec/lunatrace/bsl/ingest-worker/cmd/ingestworker/vulnerability"
+
+	"github.com/Khan/genqlient/graphql"
 	"go.uber.org/fx"
 
-	"github.com/lunasec-io/lunasec/lunatrace/bsl/ingest-worker/handlers/ingest"
+	clifx2 "github.com/ajvpot/clifx"
+
+	"github.com/lunasec-io/lunasec/lunatrace/bsl/ingest-worker/cmd/ingestworker/license"
+	"github.com/lunasec-io/lunasec/lunatrace/bsl/ingest-worker/pkg/license/scanner/licensecheck"
+	"github.com/lunasec-io/lunasec/lunatrace/bsl/ingest-worker/pkg/license/scanner/packagejson"
+	"github.com/lunasec-io/lunasec/lunatrace/bsl/ingest-worker/pkg/metadata/fetcher/npm"
+	"github.com/lunasec-io/lunasec/lunatrace/bsl/ingest-worker/pkg/metadata/ingester"
+	vulnmanager "github.com/lunasec-io/lunasec/lunatrace/bsl/ingest-worker/pkg/vulnerability"
 	"github.com/lunasec-io/lunasec/lunatrace/cli/fx/lunatracefx"
+	"github.com/lunasec-io/lunasec/lunatrace/cli/gql"
+	"github.com/lunasec-io/lunasec/lunatrace/cli/pkg/types"
 )
 
 func main() {
-	app := fx.New(
+	clifx2.Main(
 		lunatracefx.Module,
-		ingest.Module,
-	)
+		// todo make a module
+		fx.Provide(func(appConfig types.LunaTraceConfig, hc *http.Client) graphql.Client {
+			if appConfig.GraphqlServer.Url == "" {
+				log.Error().Msg("graphql server url is not defined")
+				return nil
+			}
 
-	app.Run()
-	<-app.Done()
+			if appConfig.GraphqlServer.Secret == "" {
+				log.Error().Msg("graphql server secret is not defined")
+				return nil
+			}
+
+			lhc := hc
+			lhc.Transport = &gql.HeadersTransport{Headers: map[string]string{
+				"X-Hasura-Admin-Secret": appConfig.GraphqlServer.Secret,
+				"X-Hasura-Role":         "service",
+			}}
+			return graphql.NewClient(appConfig.GraphqlServer.Url, lhc)
+		}),
+		fx.Supply(&clifx2.AppConfig{
+			Name:    "ingestworker",
+			Usage:   "LunaTrace Ingest Worker",
+			Version: "0.0.1",
+		}),
+		fx.Provide(
+			licensecheck.NewScanner,
+			packagejson.NewScanner,
+			license.NewCommand,
+			vulnmanager.NewFileIngester,
+		),
+		fx.Provide(
+			npm.NewNPMFetcher,
+			ingester.NewHasuraIngester,
+			ingest.NewCommand,
+			vulnerability.NewCommand,
+		),
+	)
 }
