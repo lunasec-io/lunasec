146595a8731929277fe0f5292c6dfb7a4ebe8ea9
breadchris <chris@lunasec.io>
2022-09-06 14:35:52 -0700

    updates
    Former-commit-id: 2546de2f14b4442c9c8b149f7e41a856857963f5
    Former-commit-id: dcbb74824b81ef34bade494e081560b390933052

--- lunatrace/bsl/backend/src/github/helpers/walk-pagination.ts
+++ lunatrace/bsl/backend/src/github/helpers/walk-pagination.ts
@@ -13,7 +13,7 @@
  */
 import { LunaLogger } from '@lunatrace/logger';
 
-const PER_PAGE = 100;
+const perPage = 100;
 
 export async function walkPagination<Item>(
   log: LunaLogger,
@@ -23,9 +23,9 @@ export async function walkPagination<Item>(
 
   // Cap at 100 iterations just because infinite loops suck.
   for (let page = 1; page < 10000; page++) {
-    log.info(`Walking github pagination #${page}`);
+    log.info('walking github pagination', { page });
 
-    const { newItems, total } = await callGithub(page, PER_PAGE);
+    const { newItems, total } = await callGithub(page, perPage);
     log.info('fetched new items while walking through github pagination: ', newItems);
     items.push(...newItems);
 
@@ -34,10 +34,10 @@ export async function walkPagination<Item>(
       return items;
     }
 
-    log.info(`Continuing to walk pagination because we havent reached the total count`);
+    log.info(`continuing to walk pagination because we haven't reached the total count`);
   }
 
-  log.error(` Hit max iterations for github pagination`);
+  log.error(`hit max iterations for github pagination`);
 
   throw new Error('Hit max iterations while walking github pagination ');
 }
