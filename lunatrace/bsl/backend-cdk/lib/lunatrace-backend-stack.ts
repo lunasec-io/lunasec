5a912a5fa188c5a450bbe08818ffe32276a203df
Free Wortley <free@lunasec.io>
2022-09-12 22:58:05 -0500

    Revert "Merge pull request #875 from lunasec-io/disable-hasura-health-checks"
    This reverts commit ffc8b38069c66014df2b56c8a0e6a0d7eb4a0873 [formerly 2c118ad11b791187f6eb5277a89327700e56431a] [formerly 06eb8e67a997cfdb84953d4939ba62d48d07efc3], reversing
    changes made to c3bb6fee7c5f954fa9a4c0f436623ced0e91d4e9 [formerly 46f23f484dbdb75c66f3bcd0cbfabaecc06c3ee2] [formerly 996c103279d5b4c986a4a9677a82c8918fc1d6b8].
    
    
    Former-commit-id: cca2a82bbd6f71aea288107490f5a19840411bca
    Former-commit-id: 977418189468b5858324f8e133fc5c36d113b298

--- lunatrace/bsl/backend-cdk/lib/lunatrace-backend-stack.ts
+++ lunatrace/bsl/backend-cdk/lib/lunatrace-backend-stack.ts
@@ -290,10 +290,10 @@ export class LunatraceBackendStack extends cdk.Stack {
         HASURA_GRAPHQL_DATABASE_URL: EcsSecret.fromSecretsManager(hasuraDatabaseUrlSecret),
         HASURA_GRAPHQL_ADMIN_SECRET: EcsSecret.fromSecretsManager(hasuraAdminSecret),
       },
-      // healthCheck: {
-      //   // TODO: Make this verify a 200 status code in the response
-      //   command: ['CMD-SHELL', 'wget --no-verbose --tries=1 --spider http://localhost:8080/healthz || exit 1'],
-      // },
+      healthCheck: {
+        // TODO: Make this verify a 200 status code in the response
+        command: ['CMD-SHELL', 'wget --no-verbose --tries=1 --spider http://localhost:8080/healthz || exit 1'],
+      },
     });
 
     backend.addContainerDependencies({
