3b02269ff11805128ccc558ea0be80a2a4072c52
breadchris <chris@lunasec.io>
2022-09-19 19:41:16 -0700

    more test cases for semgrep rule

--- lunatrace/bsl/semgrep/code/imported-and-called/match/esmodule/default-export-mix.js
+++ lunatrace/bsl/semgrep/code/imported-and-called/match/esmodule/default-export-mix.js
@@ -11,11 +11,6 @@
  * limitations under the License.
  *
  */
-import { export1 as alias1 } from "module-name";
-import { default as alias } from "module-name";
-import { export1, export2 } from "module-name";
-import { export1, export2 as alias2, /* … */ } from "module-name";
-import { "string name" as alias } from "module-name";
-import defaultExport, { export1, /* … */ } from "module-name";
-import defaultExport, * as name from "module-name";
-import "module-name";
+import vulnerable, { export1, /* … */ } from "vulnerable-package";
+
+vulnerable();
