fdf6519a22edee6c9ff51e8cea1d183a250f2fc7
breadchris <chris@lunasec.io>
2022-09-19 17:59:59 -0700

    add test cases for semgrep
    Former-commit-id: d12834222cebce94ca6aaab6be5fac74a98ef17b
    Former-commit-id: 0c7964ee54237885f5810e21c2dcb9a541c0493c

---
+++ lunatrace/bsl/semgrep/code/js/esmodule/esmodule-edge-cases.js
@@ -0,0 +1,21 @@
+/*
+ * Copyright by LunaSec (owned by Refinery Labs, Inc)
+ *
+ * Licensed under the Business Source License v1.1 
+ * (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ * https://github.com/lunasec-io/lunasec/blob/master/licenses/BSL-LunaTrace.txt
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+import { export1 as alias1 } from "module-name";
+import { default as alias } from "module-name";
+import { export1, export2 } from "module-name";
+import { export1, export2 as alias2, /* … */ } from "module-name";
+import { "string name" as alias } from "module-name";
+import defaultExport, { export1, /* … */ } from "module-name";
+import defaultExport, * as name from "module-name";
+import "module-name";
