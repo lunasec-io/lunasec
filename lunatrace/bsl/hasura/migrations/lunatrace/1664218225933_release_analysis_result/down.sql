7777b35a206d3e32b421a5c3cecc1cda835f517c
Alex Vanderpot <alex@lunasec.io>
2022-09-26 11:50:38 -0700

    schema
    Former-commit-id: d9d5426b3f1b7f11d293d1fd642196f24040e43c
    Former-commit-id: d4958295de49b8b98c8f8528ee897efed6ea592f

---
+++ lunatrace/bsl/hasura/migrations/lunatrace/1664218225933_release_analysis_result/down.sql
@@ -0,0 +1,19 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- CREATE TYPE "finding_state" AS ENUM (
+--   'vulnerable',
+--   'not_vulnerable'
+-- );
+--
+-- CREATE TABLE "package"."release_analysis_result" (
+--   "id" uuid PRIMARY KEY DEFAULT (gen_random_uuid()),
+--   "manifest_dependency_edge_id" uuid,
+--   "vulnerability_id" uuid,
+--   "state" finding_state,
+--   "created_at" timestamptz,
+--   "source" TEXT
+-- );
+--
+-- ALTER TABLE "package"."release_analysis_result" ADD FOREIGN KEY ("manifest_dependency_edge_id") REFERENCES "manifest_dependency_edge" ("id");
+--
+-- ALTER TABLE "package"."release_analysis_result" ADD FOREIGN KEY ("vulnerability_id") REFERENCES "vulnerability"."vulnerability" ("id");
