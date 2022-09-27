a82937030339c8aa1e0931353c27d06ba5bf00cf
Alex Vanderpot <alex@lunasec.io>
2022-09-27 12:00:29 -0700

    down migration, fmt

--- lunatrace/bsl/hasura/migrations/lunatrace/1664218225933_release_analysis_result/up.sql
+++ lunatrace/bsl/hasura/migrations/lunatrace/1664218225933_release_analysis_result/up.sql
@@ -1,17 +1,20 @@
-CREATE TYPE "finding_state" AS ENUM (
-  'vulnerable',
-  'not_vulnerable'
+CREATE TYPE finding_state AS ENUM (
+    'vulnerable',
+    'not_vulnerable'
+    );
+
+CREATE TABLE package.release_analysis_result
+(
+    id                          uuid PRIMARY KEY DEFAULT (gen_random_uuid()),
+    manifest_dependency_edge_id uuid,
+    vulnerability_id            uuid,
+    state                       finding_state,
+    created_at                  timestamptz,
+    source                      TEXT
 );
 
-CREATE TABLE "package"."release_analysis_result" (
-  "id" uuid PRIMARY KEY DEFAULT (gen_random_uuid()),
-  "manifest_dependency_edge_id" uuid,
-  "vulnerability_id" uuid,
-  "state" finding_state,
-  "created_at" timestamptz,
-  "source" TEXT
-);
+ALTER TABLE package.release_analysis_result
+    ADD FOREIGN KEY (manifest_dependency_edge_id) REFERENCES manifest_dependency_edge (id);
 
-ALTER TABLE "package"."release_analysis_result" ADD FOREIGN KEY ("manifest_dependency_edge_id") REFERENCES "manifest_dependency_edge" ("id");
-
-ALTER TABLE "package"."release_analysis_result" ADD FOREIGN KEY ("vulnerability_id") REFERENCES "vulnerability"."vulnerability" ("id");
+ALTER TABLE package.release_analysis_result
+    ADD FOREIGN KEY (vulnerability_id) REFERENCES vulnerability.vulnerability (id);
