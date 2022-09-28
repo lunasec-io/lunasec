04d5f52a41ea4827a19ed65dd276b58403d5d107
Alex Vanderpot <alex@lunasec.io>
2022-09-27 18:06:01 -0700

    revise schema
    Former-commit-id: 8e8e9db3f124c97efd4256ce9fef203a12c15605
    Former-commit-id: 485b389342348f03ff3efe56d00e9da0d39b2de0

--- lunatrace/bsl/hasura/migrations/lunatrace/1664218225933_release_analysis_result/up.sql
+++ lunatrace/bsl/hasura/migrations/lunatrace/1664218225933_release_analysis_result/up.sql
@@ -1,20 +1,37 @@
-CREATE TYPE finding_state AS ENUM (
-    'vulnerable',
-    'not_vulnerable'
-    );
+CREATE SCHEMA analysis;
 
-CREATE TABLE package.release_analysis_result
+CREATE TABLE analysis.finding_type
 (
-    id                          uuid PRIMARY KEY DEFAULT (gen_random_uuid()),
-    manifest_dependency_edge_id uuid,
-    vulnerability_id            uuid,
-    state                       finding_state,
-    created_at                  timestamptz,
-    source                      TEXT
+    value TEXT PRIMARY KEY
 );
 
-ALTER TABLE package.release_analysis_result
+INSERT INTO analysis.finding_type (value)
+VALUES ('not_vulnerable'),
+       ('vulnerable'),
+       ('not_vulnerable_not_imported'),
+       ('not_vulnerable_not_called');
+
+CREATE TABLE analysis.finding_source
+(
+    value TEXT PRIMARY KEY
+);
+
+INSERT INTO analysis.finding_source (value)
+VALUES ('semgrep_imported_called');
+
+
+CREATE TABLE analysis.manifest_dependency_edge_result
+(
+    id                          uuid PRIMARY KEY DEFAULT (gen_random_uuid()),
+    manifest_dependency_edge_id uuid        NOT NULL,
+    vulnerability_id            uuid        NOT NULL,
+    finding_type                TEXT        NOT NULL REFERENCES analysis.finding_type,
+    finding_source              TEXT        NOT NULL REFERENCES analysis.finding_type,
+    created_at                  timestamptz NOT NULL
+);
+
+ALTER TABLE analysis.manifest_dependency_edge_result
     ADD FOREIGN KEY (manifest_dependency_edge_id) REFERENCES manifest_dependency_edge (id);
 
-ALTER TABLE package.release_analysis_result
+ALTER TABLE analysis.manifest_dependency_edge_result
     ADD FOREIGN KEY (vulnerability_id) REFERENCES vulnerability.vulnerability (id);
