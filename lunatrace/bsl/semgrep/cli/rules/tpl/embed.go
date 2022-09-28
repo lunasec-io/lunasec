31318e7c7faf889118013808daea375eef83d90f
Alex Vanderpot <alex@lunasec.io>
2022-09-28 12:48:57 -0700

    move rule to file

--- lunatrace/bsl/semgrep/cli/rules/tpl/embed.go
+++ lunatrace/bsl/semgrep/cli/rules/tpl/embed.go
@@ -1,6 +1,6 @@
 // Copyright by LunaSec (owned by Refinery Labs, Inc)
 //
-// Licensed under the Business Source License v1.1 
+// Licensed under the Business Source License v1.1
 // (the "License"); you may not use this file except in compliance with the
 // License. You may obtain a copy of the License at
 //
@@ -8,14 +8,13 @@
 //
 // See the License for the specific language governing permissions and
 // limitations under the License.
-//
 package tpl
 
 import (
 	"embed"
 )
 
-// content is our static web server content.
+// embed all files in this directory to the module variable RuleTemplates
 //
 //go:embed *
 var RuleTemplates embed.FS
