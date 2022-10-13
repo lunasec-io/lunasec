17bf6ed432cbcca532b0b9f4d0a718b062046ef1
breadchris <chris@lunasec.io>
2022-10-13 14:40:32 -0700

    replication by default starts at the beginning

--- lunatrace/bsl/ingest-worker/pkg/metadata/replicator.go
+++ lunatrace/bsl/ingest-worker/pkg/metadata/replicator.go
@@ -3,5 +3,6 @@ package metadata
 import "context"
 
 type Replicator interface {
-	Replicate(ctx context.Context, offset, limit int) error
+	ReplicateSince(ctx context.Context, offset int) error
+	InitialReplication(ctx context.Context) error
 }
