3e617c59a6e12328499d87bb760fd992ffbfd827
breadchris <chris@lunasec.io>
2022-10-12 23:32:44 -0700

    fix hasura
    Former-commit-id: 3d39460d6e69a095a68a50ca5a6be786569633d9
    Former-commit-id: 34c25314a73a9f7a8d51bb70d9d9bffb8739dc5c

--- lunatrace/bsl/hasura/migrations/lunatrace/1665530343339_npm_registry_replicator/up.sql
+++ lunatrace/bsl/hasura/migrations/lunatrace/1665530343339_npm_registry_replicator/up.sql
@@ -1,10 +1,10 @@
 CREATE SCHEMA "npm";
 
 CREATE TABLE "npm"."revision" (
-    "rev" TEXT,
-    "id" TEXT,
-    "doc" JSONB,
-    "deleted" BOOLEAN,
+    "rev" TEXT NOT NULL,
+    "id" TEXT NOT NULL,
+    "doc" JSONB NOT NULL,
+    "deleted" BOOLEAN NOT NULL,
     "seq" INTEGER PRIMARY KEY
 );
 COMMENT ON TABLE "npm"."revision" IS E'Revision of a NPM CouchDB document.';
