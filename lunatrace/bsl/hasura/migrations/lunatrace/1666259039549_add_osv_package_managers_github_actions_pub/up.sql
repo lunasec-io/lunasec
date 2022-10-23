4147212a08ccd77e56492ddd779d94420a50e14f
breadchris <chris@lunasec.io>
2022-10-23 10:07:46 -0700

    update migration

--- lunatrace/bsl/hasura/migrations/lunatrace/1666259039549_add_osv_package_managers_github_actions_pub/up.sql
+++ lunatrace/bsl/hasura/migrations/lunatrace/1666259039549_add_osv_package_managers_github_actions_pub/up.sql
@@ -1,4 +1,2 @@
 ALTER TYPE public.package_manager ADD VALUE IF NOT EXISTS 'github_actions';
 ALTER TYPE public.package_manager ADD VALUE IF NOT EXISTS 'pub';
-
-ALTER TABLE package.release_dependency DROP COLUMN dependency_release_id;
