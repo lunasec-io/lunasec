85caf86509f4905ba668ebd9be62e5589393ba9c
breadchris <chris@lunasec.io>
2022-10-26 09:17:08 -0700

    update rule

--- lunatrace/bsl/semgrep/code/imported-and-called/match/commonjs/react.js
+++ lunatrace/bsl/semgrep/code/imported-and-called/match/commonjs/react.js
@@ -3,4 +3,4 @@ var _react = require('vulnerable-package');
 var _react2 = _interopRequireDefault(_react);
 
 // ruleid: imported-and-called-taint
-var content = asdf.default.createElement();
+var content = _react2.default.createElement();
