85ad057835cfd98ac37006408f8fb822bb5aedd2
breadchris <chris@lunasec.io>
2022-11-08 22:30:52 -0800

    faster tree

--- lunatrace/bsl/backend/src/tests/dependency-tree.test.ts
+++ lunatrace/bsl/backend/src/tests/dependency-tree.test.ts
@@ -8,24 +8,24 @@ import { fakeDependencyTreeHasuraOutputFixture } from './fixtures/manifests/fake
 import { realDependencyTreeHasuraOutputFixture } from './fixtures/manifests/real-dependency-tree-hasura-output';
 
 describe('The fake dependency tree', () => {
+  const rawTreeString = fs
+    .readFileSync(path.join(__dirname, 'fixtures/manifests/huge-docusaurus-tree-hasura-output.json'))
+    .toString();
+  const parsedTreeData = JSON.parse(rawTreeString) as Manifest[];
+
   it('should generate a dependency tree from fake dependency tree', () => {
-    const tree = new VulnerabilityDependencyTree(fakeDependencyTreeHasuraOutputFixture);
+    const tree = new VulnerabilityDependencyTree(fakeDependencyTreeHasuraOutputFixture as Array<Manifest>);
     const vulnerableReleases = tree.getVulnerableReleases();
     expect(vulnerableReleases.length).toBe(1);
   });
   it('should generate a dependency tree from real dependency tree', () => {
-    const tree = new VulnerabilityDependencyTree(realDependencyTreeHasuraOutputFixture);
+    const tree = new VulnerabilityDependencyTree(realDependencyTreeHasuraOutputFixture as Array<Manifest>);
     const vulnerableReleases = tree.getVulnerableReleases();
     expect(vulnerableReleases.length).toBe(48);
   });
   it('should generate a dependency tree from a very large dependency tree', () => {
-    const rawTreeString = fs
-      .readFileSync(path.join(__dirname, '../fixtures/manifests/huge-docusaurus-tree-hasura-output.json'))
-      .toString();
-    const parsedTreeData = JSON.parse(rawTreeString) as Manifest[];
-
     const tree = new VulnerabilityDependencyTree(parsedTreeData);
     const vulnerableReleases = tree.getVulnerableReleases();
-    expect(vulnerableReleases.length).toBe(48);
+    expect(vulnerableReleases.length).toBe(17);
   });
 });
