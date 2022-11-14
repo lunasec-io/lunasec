cfad2d3e1392bc0a3140d42dfa1f9da6aaf41916
breadchris <chris@lunasec.io>
2022-11-14 15:14:48 -0800

    dependency chains were not being correctly made

--- lunatrace/bsl/backend/src/tests/dependency-tree.test.ts
+++ lunatrace/bsl/backend/src/tests/dependency-tree.test.ts
@@ -1,6 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 
+import { DependencyTree } from '../models/dependency-tree/builds-dependency-tree';
 import VulnerabilityDependencyTree from '../models/vulnerability-dependency-tree';
 import { Manifest } from '../models/vulnerability-dependency-tree/types';
 
@@ -29,5 +30,24 @@ describe('The fake dependency tree', () => {
     const tree = new VulnerabilityDependencyTree(parsedTreeData);
     const vulnerableReleases = tree.getVulnerableReleases();
     expect(vulnerableReleases.length).toBe(17);
+
+    const vulnerableEdges = tree.getEdgesWhereChildIsVulnerable();
+    console.log(vulnerableEdges.length);
+  });
+
+  it('should generate a dependency tree from a very large dependency tree1', () => {
+    const rawTreeString = fs
+      .readFileSync(path.join(__dirname, 'fixtures/manifests/huge-docusaurus-tree-hasura-output.json'))
+      .toString();
+    const parsedTreeData = JSON.parse(rawTreeString) as Manifest[];
+
+    parsedTreeData.forEach((t) => t.child_edges_recursive?.forEach((e) => (e.analysis_results = [])));
+
+    const tree = new DependencyTree(parsedTreeData, null, []);
+    const vulnerableReleases = tree.vulnerableReleases;
+    expect(vulnerableReleases.length).toBe(17);
+
+    const vulnerableEdges = tree.getVulnerabilities();
+    console.log(vulnerableEdges.length);
   });
 });
