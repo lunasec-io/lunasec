b3f5f9e8405b3c87e9a3a5d3ebc822a23ae07f57
Chris <chris@lunasec.io>
2022-12-16 17:25:18 -0800

    adds video to authed and unauthed views (#1057)
    ![screenshot-localhost_4455-2022 12
    16-17_07_49](https://user-images.githubusercontent.com/1069294/208215310-428aa3b0-1432-488f-8e30-1564c9b4be46.png)
    
    Video explains how to use lunatrace.
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Former-commit-id: f26f256f42abe546ebd104a39c7ac84f73123b67
    Former-commit-id: 5eb935c51b22c7127dcbd352b551d8ab621262b5

---
+++ lunatrace/bsl/frontend/src/components/LunaTraceIntroVideo.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const LunaTraceIntroVideo = () => {
+  return (
+    <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/ugdSyR2L6sY" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+  )
+}
