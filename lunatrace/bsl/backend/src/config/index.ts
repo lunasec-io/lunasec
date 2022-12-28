4d0decf1bdd792f89c3dabc80f2e9cd89b0af269
Forrest <forrest@lunasec.io>
2022-12-27 16:22:18 -0800

    new config system for env vars

---
+++ lunatrace/bsl/backend/src/config/index.ts
@@ -0,0 +1,155 @@
+/*
+ * Copyright by LunaSec (owned by Refinery Labs, Inc)
+ *
+ * Licensed under the Business Source License v1.1
+ * (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ * https://github.com/lunasec-io/lunasec/blob/master/licenses/BSL-LunaTrace.txt
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+import {
+  AwsConfig,
+  GithubAppConfig,
+  HasuraConfig,
+  JwksConfig,
+  LogConfig,
+  QueueHandlerConfig,
+  RepositoryQueueConfig,
+  ServerConfig,
+  SqsQueueConfig,
+  StaticAnalysisConfig,
+  WebhookConfig,
+  WorkerBucketConfig,
+} from '../types/config';
+import { tryParseInt, validateBooleanString } from '../utils/parse';
+
+import { envVars } from './load-environment-vars';
+
+export { envVars } from './load-environment-vars';
+export const isProduction = envVars.NodeEnv === 'production';
+
+// The below loaders build configs for various parts of the app.
+// TODO: There is no reason in node to load configs like this in NodeJS, its just extra code and not real DI.
+// TODO: We should move to code loading the parsed envVars directly, and if we are doing a lot of type casting, build that
+// TODO: into the env var loader in load-environment-vars in a generic way. These loaders are left here pending a future refactor.
+export function getLogConfig(): LogConfig {
+  const enableLogIOLogging = validateBooleanString(envVars.EnableLogIOLogging);
+  if (enableLogIOLogging.error) {
+    throw new Error(enableLogIOLogging.msg);
+  }
+
+  return {
+    enableLogIOLogging: enableLogIOLogging.res,
+    logFilePath: envVars.LogFilePath,
+    loggerName: envVars.WorkerType,
+  };
+}
+
+export function getServerConfig(): ServerConfig {
+  const serverPort = parseInt(envVars.Port, 10);
+
+  return {
+    serverPort,
+    sitePublicUrl: envVars.SitePublicUrl,
+    isProduction: envVars.NodeEnv === 'production',
+  };
+}
+
+export function getAwsConfig(): AwsConfig {
+  return {
+    awsRegion: envVars.AwsDefaultRegion,
+  };
+}
+
+export function getWebhookConfig(): WebhookConfig {
+  const disableWebhookQueue = validateBooleanString(envVars.DisableWebhookQueue);
+  if (disableWebhookQueue.error) {
+    throw new Error(disableWebhookQueue.msg);
+  }
+
+  return {
+    disableWebhookQueue: disableWebhookQueue.res,
+    queueName: envVars.ProcessWebhookQueue,
+    secret: envVars.GithubAppWebhookSecret,
+  };
+}
+
+export function getRepositoryQueueConfig(): RepositoryQueueConfig {
+  return {
+    queueName: envVars.ProcessRepositoryQueue,
+  };
+}
+
+export function getHasuraConfig(): HasuraConfig {
+  return {
+    hasuraEndpoint: envVars.HasuraEndpoint,
+    staticAccessToken: envVars.StaticAccessToken,
+  };
+}
+
+export function getWorkerBucketConfig(): WorkerBucketConfig {
+  return {
+    sbomBucket: envVars.S3SbomBucket,
+    manifestBucket: envVars.S3ManifestBucket,
+  };
+}
+
+export function getStaticAnalysisConfig(): StaticAnalysisConfig {
+  return {
+    queueName: envVars.StaticAnalysisQueueName,
+  };
+}
+
+export function getQueueHandlerConfig(): SqsQueueConfig {
+  const queueMaxMessagesEnv = envVars.QueueMaxMessages;
+  const queueVisibilityEnv = envVars.queueVisibility;
+
+  const maxMessages = tryParseInt(queueMaxMessagesEnv, 10);
+  const visibility = tryParseInt(queueVisibilityEnv, 10);
+
+  if (!maxMessages.success) {
+    throw new Error(`Queue max messages is not a valid integer: ${queueMaxMessagesEnv}`);
+  }
+
+  if (!visibility.success) {
+    throw new Error(`Queue visibility is not a valid integer: ${queueVisibilityEnv}`);
+  }
+
+  const handlerConfig: QueueHandlerConfig = {
+    maxMessages: maxMessages.value,
+    visibility: visibility.value,
+  };
+
+  const queueName = envVars.QueueName;
+
+  return {
+    handlerConfig,
+    queueName,
+  };
+}
+
+export function getGithubAppConfig(): GithubAppConfig {
+  const githubPrivateKeyRaw = envVars.GithubAppPrivateKey;
+  const githubPrivateKey = Buffer.from(githubPrivateKeyRaw, 'base64').toString('utf-8');
+
+  const githubAppIdRaw = envVars.GithubAppId;
+  const githubAppId = parseInt(githubAppIdRaw, 10);
+
+  return {
+    githubAppId,
+    githubPrivateKey,
+    githubEndpoint: envVars.GithubEndpoint,
+  };
+}
+
+export function getJwksConfig(): JwksConfig {
+  return {
+    jwksUri: envVars.JwksUri,
+    jwksIssuer: envVars.JwksIssuer,
+  };
+}
---
+++ lunatrace/bsl/backend/src/config/load-environment-vars.ts
@@ -0,0 +1,161 @@
+/*
+ * Copyright by LunaSec (owned by Refinery Labs, Inc)
+ *
+ * Licensed under the Business Source License v1.1
+ * (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ * https://github.com/lunasec-io/lunasec/blob/master/licenses/BSL-LunaTrace.txt
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+import dotenv from 'dotenv';
+
+dotenv.config({ path: process.env.NODE_ENV === 'production' ? '.env' : '.env.dev' });
+
+const checkEnvVar = (envVarKey: string, backupEnvVarKey?: string, defaultValue?: string) => {
+  const envVar = process.env[envVarKey];
+  const backupVar = backupEnvVarKey ? process.env[backupEnvVarKey] : null;
+  // If the environment variable is not set, and the value must come from the environment,
+  // AND we are in production and the default value is not defined.
+  // then throw an error
+  if (!envVar && !backupVar && defaultValue === undefined) {
+    throw new Error(`Missing ${envVarKey} env var ${backupEnvVarKey ? `and backup var ${backupEnvVarKey}` : ''}`);
+  }
+  return envVar || backupVar || (defaultValue as string);
+};
+
+const commonEnvVarKeys = {
+  NodeEnv: {
+    varKey: 'NODE_ENV',
+    defaultValue: 'development',
+  },
+  EnableLogIOLogging: {
+    varKey: 'ENABLE_LOGIO_LOGGING',
+    defaultValue: 'false',
+  },
+  LogFilePath: {
+    varKey: 'LOG_FILE_PATH',
+  },
+  WorkerType: {
+    varKey: 'WORKER_TYPE',
+    defaultValue: 'lunatrace-backend',
+  },
+  Port: {
+    varKey: 'PORT',
+    defaultValue: '3002',
+  },
+  SitePublicUrl: {
+    varKey: 'SITE_PUBLIC_URL',
+    defaultValue: 'http://localhost:4455/',
+  },
+  AwsDefaultRegion: {
+    varKey: 'AWS_DEFAULT_REGION',
+    defaultValue: 'us-west-2',
+  },
+  DisableWebhookQueue: {
+    varKey: 'DISABLE_WEBHOOK_QUEUE',
+    defaultValue: 'false',
+  },
+  QueueName: {
+    varKey: 'QUEUE_NAME',
+  },
+  ProcessWebhookQueue: {
+    varKey: 'PROCESS_WEBHOOK_QUEUE',
+    backupVarKey: 'QUEUE_NAME',
+  },
+  ProcessRepositoryQueue: {
+    varKey: 'PROCESS_REPOSITORY_QUEUE',
+    backupVarKey: 'QUEUE_NAME',
+  },
+
+  JwksUri: {
+    varKey: 'JWKS_URI',
+    defaultValue: 'http://localhost:4456/.well-known/jwks.json',
+  },
+  HasuraEndpoint: {
+    varKey: 'HASURA_URL',
+    defaultValue: 'http://localhost:4455/api/service/v1/graphql',
+  },
+  StaticAccessToken: {
+    varKey: 'STATIC_SECRET_ACCESS_TOKEN',
+    defaultValue: 'fc7efb9e-04e0-4883-b7b4-8f2e86d1e2e1',
+  },
+  StaticAnalysisQueueName: {
+    varKey: 'STATIC_ANALYSIS_QUEUE',
+    backupVarKey: 'GOLANG_QUEUE_NAME',
+  },
+  QueueMaxMessages: {
+    varKey: 'QUEUE_MAX_MESSAGES',
+    defaultValue: '10',
+  },
+  queueVisibility: {
+    varKey: 'QUEUE_VISIBILITY',
+    defaultValue: '60',
+  },
+  GithubEndpoint: {
+    varKey: 'GITHUB_ENDPOINT',
+    defaultValue: 'https://api.github.com/graphql',
+  },
+  JwksIssuer: {
+    varKey: 'JWKS_ISSUER',
+    defaultValue: 'http://oathkeeper:4455/',
+  },
+  S3SbomBucket: {
+    varKey: 'S3_SBOM_BUCKET',
+  },
+  S3ManifestBucket: {
+    varKey: 'S3_MANIFEST_BUCKET',
+  },
+  GithubAppId: {
+    varKey: 'GITHUB_APP_ID',
+  },
+  GithubAppPrivateKey: {
+    varKey: 'GITHUB_APP_PRIVATE_KEY',
+  },
+  GithubAppWebhookSecret: {
+    varKey: 'GITHUB_APP_WEBHOOK_SECRET',
+    defaultValue: 'mysecret',
+  },
+};
+
+// TODO: I can't figure out where these are referenced in the existing codebase, maybe chatgpt made them up
+// const workerEnvVarKeys = {
+//   WebhookExecutionRoleArn: {
+//     varKey: 'WEBHOOK_EXECUTION_ROLE_ARN',
+//   },
+//   WorkerBucketName: {
+//     varKey: 'WORKER_BUCKET_NAME',
+//   },
+//   GithubAppClientId: {
+//     varKey: 'GITHUB_APP_CLIENT_ID',
+//   },
+//   GithubAppClientSecret: {
+//     varKey: 'GITHUB_APP_CLIENT_SECRET',
+//   },
+// GraphqlEndpoint: {
+//   varKey: 'GRAPHQL_ENDPOINT',
+// },
+// GraphqlAdminSecret: {
+//   varKey: 'GRAPHQL_ADMIN_SECRET',
+// },
+// };
+
+type VarName = keyof typeof commonEnvVarKeys;
+type EnvVars = Record<VarName, string>;
+interface EnvVar {
+  varKey: string;
+  backupVarKey?: string;
+  defaultValue?: string;
+}
+
+const partialEnvironmentVars: Partial<EnvVars> = {};
+Object.keys(commonEnvVarKeys).forEach((keyName) => {
+  const varName = keyName as VarName;
+  const varConf: EnvVar = commonEnvVarKeys[varName];
+  partialEnvironmentVars[varName] = checkEnvVar(varConf.varKey, varConf.backupVarKey, varConf.defaultValue);
+});
+
+export const envVars = partialEnvironmentVars as EnvVars;
