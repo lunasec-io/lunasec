3a4952ac4b92cbe93add4dacdac615827563ecdc
breadchris <chris@lunasec.io>
2023-03-02 08:16:53 -0800

    embed with backoff

--- lunatrace/bsl/ingest-worker/pkg/ml/embeddings.go
+++ lunatrace/bsl/ingest-worker/pkg/ml/embeddings.go
@@ -11,6 +11,7 @@ import (
 	"strings"
 
 	"github.com/PullRequestInc/go-gpt3"
+	"github.com/cenkalti/backoff/v4"
 	"github.com/go-jet/jet/v2/postgres"
 	"github.com/google/uuid"
 	"github.com/pkg/errors"
@@ -185,16 +186,146 @@ func (p *service) SearchForReferences(search, vulnID string) (string, error) {
 	return p.AnswerQuestionFromContent(VulnerabilityReferencePrompt, search, content)
 }
 
+type ReferenceContentResp struct {
+	model.Reference
+	model.ReferenceContent
+	model.Vulnerability
+}
+
+func (p *service) generateEmbeddingForRef(ref *ReferenceContentResp, insertWithPinecone bool) error {
+	// Build the Content to embed by combining the vulnerability ID, Title, and Content
+	content := fmt.Sprintf("%s %s %s", ref.Vulnerability.SourceID, ref.Title, ref.NormalizedContent)
+
+	// Split ref normalized Content into words, group in chunks of 1024 words
+	words := strings.Split(content, " ")
+	var chunks [][]string
+	for i := 0; i < len(words); i += 1024 {
+		end := i + 1024
+		if end > len(words) {
+			end = len(words)
+		}
+		chunks = append(chunks, words[i:end])
+	}
+
+	var pineconeVectors []*pineconefx.Vector
+
+	for _, chunk := range chunks {
+		formattedChunk := strings.Join(chunk, " ")
+
+		// calculate the hash of the chunk
+		hash := sha256.Sum256([]byte(formattedChunk))
+		hashStr := hex.EncodeToString(hash[:])
+
+		var re = table.ReferenceEmbedding
+		getExistingRefEmb := re.SELECT(
+			re.Embedding,
+		).WHERE(re.ContentHash.EQ(postgres.String(hashStr)))
+
+		var refEmb model.ReferenceEmbedding
+		err := getExistingRefEmb.Query(p.deps.DB, &refEmb)
+		if err == nil {
+			// skip this chunk, we already have an embedding for it
+			log.Info().Str("url", ref.URL).Msg("skipping chunk, already have embedding")
+
+			var embedding []float64
+			err = json.Unmarshal([]byte(refEmb.Embedding), &embedding)
+			if err != nil {
+				log.Error().Err(err).Msg("failed to unmarshal embedding")
+				return err
+			}
+
+			if insertWithPinecone {
+				pineconeVectors = append(pineconeVectors, newVulnRefVector(
+					hashStr,
+					embedding,
+					ref.Reference.URL,
+					ref.Vulnerability.SourceID,
+				))
+			}
+			continue
+		}
+
+		req := gpt3.EmbeddingsRequest{
+			Input: []string{formattedChunk},
+			Model: gpt3.TextEmbeddingAda002,
+		}
+
+		res, err := p.deps.OpenAIClient.Embeddings(context.Background(), req)
+		if err != nil {
+			log.Error().
+				Err(err).
+				Str("url", ref.URL).
+				Msg("failed to generate embedding")
+			return err
+		}
+
+		// TODO (cthompson) will there be more than just one value in Data? currently we are only sending one chunk at a time.
+		embedding := res.Data[0].Embedding
+		embeddingData, err := json.Marshal(embedding)
+		if err != nil {
+			log.Error().
+				Err(err).
+				Str("url", ref.URL).
+				Msg("failed to marshal embedding data")
+			return err
+		}
+
+		newRefEmb := model.ReferenceEmbedding{
+			ReferenceContentID: ref.ReferenceContent.ID,
+			ContentHash:        hashStr,
+			Content:            formattedChunk,
+			Embedding:          string(embeddingData),
+		}
+
+		insertStmt := re.INSERT(
+			re.ReferenceContentID, re.ContentHash, re.Content, re.Embedding,
+		).MODEL(newRefEmb)
+
+		_, err = insertStmt.Exec(p.deps.DB)
+		if err != nil {
+			log.Error().
+				Err(err).
+				Msg("failed to insert reference embedding")
+			return err
+		}
+
+		if insertWithPinecone {
+			pineconeVectors = append(pineconeVectors, newVulnRefVector(hashStr, embedding, ref.URL, ref.Vulnerability.SourceID))
+
+			// 20 is used in the Buff script, why is it 20?
+			if len(pineconeVectors) == 20 {
+				log.Info().
+					Str("url", ref.URL).
+					Msg("upserting embedding to pinecone")
+
+				err = p.deps.PineconeClient.Upsert(pineconeVectors)
+				if err != nil {
+					log.Error().
+						Err(err).
+						Msg("failed to upsert pinecone vectors")
+					return err
+				}
+				pineconeVectors = []*pineconefx.Vector{}
+			}
+		}
+	}
+
+	if insertWithPinecone {
+		// upsert the remaining vectors to pinecone
+		err := p.deps.PineconeClient.Upsert(pineconeVectors)
+		if err != nil {
+			log.Error().
+				Err(err).
+				Msg("failed to upsert pinecone vectors")
+			return err
+		}
+	}
+}
+
 func (p *service) GenerateEmbeddingsForVulnRefs(vulnID string, insertWithPinecone bool) error {
 	rc := table.ReferenceContent
 	r := table.Reference
 	v := table.Vulnerability
-
-	type ReferenceContentResp struct {
-		model.Reference
-		model.ReferenceContent
-		model.Vulnerability
-	}
 
 	getReferenceContentStmt := rc.SELECT(rc.AllColumns, r.AllColumns, v.ID).
 		FROM(
@@ -212,7 +343,6 @@ func (p *service) GenerateEmbeddingsForVulnRefs(vulnID string, insertWithPinecon
 		return err
 	}
 
-	var pineconeVectors []*pineconefx.Vector
 	for rows.Next() {
 		var ref ReferenceContentResp
 		err = rows.Scan(&ref)
@@ -239,130 +369,16 @@ func (p *service) GenerateEmbeddingsForVulnRefs(vulnID string, insertWithPinecon
 			Str("url", ref.URL).
 			Msg("generating embedding for reference")
 
-		// Build the Content to embed by combining the vulnerability ID, Title, and Content
-		content := fmt.Sprintf("%s %s %s", ref.Vulnerability.SourceID, ref.Title, ref.NormalizedContent)
-
-		// Split ref normalized Content into words, group in chunks of 1024 words
-		words := strings.Split(content, " ")
-		var chunks [][]string
-		for i := 0; i < len(words); i += 1024 {
-			end := i + 1024
-			if end > len(words) {
-				end = len(words)
-			}
-			chunks = append(chunks, words[i:end])
-		}
-
-		for _, chunk := range chunks {
-			formattedChunk := strings.Join(chunk, " ")
-
-			// calculate the hash of the chunk
-			hash := sha256.Sum256([]byte(formattedChunk))
-			hashStr := hex.EncodeToString(hash[:])
-
-			var re = table.ReferenceEmbedding
-			getExistingRefEmb := re.SELECT(
-				re.Embedding,
-			).WHERE(re.ContentHash.EQ(postgres.String(hashStr)))
-
-			var refEmb model.ReferenceEmbedding
-			err = getExistingRefEmb.Query(p.deps.DB, &refEmb)
-			if err == nil {
-				// skip this chunk, we already have an embedding for it
-				log.Info().Str("url", ref.URL).Msg("skipping chunk, already have embedding")
-
-				var embedding []float64
-				err = json.Unmarshal([]byte(refEmb.Embedding), &embedding)
-				if err != nil {
-					log.Error().Err(err).Msg("failed to unmarshal embedding")
-					return err
-				}
-
-				if insertWithPinecone {
-					pineconeVectors = append(pineconeVectors, newVulnRefVector(
-						hashStr,
-						embedding,
-						ref.Reference.URL,
-						ref.Vulnerability.SourceID,
-					))
-				}
-				continue
-			}
-
-			req := gpt3.EmbeddingsRequest{
-				Input: []string{formattedChunk},
-				Model: gpt3.TextEmbeddingAda002,
-			}
-
-			res, err := p.deps.OpenAIClient.Embeddings(context.Background(), req)
-			if err != nil {
-				log.Error().
-					Err(err).
-					Str("url", ref.URL).
-					Msg("failed to generate embedding")
-				return err
-			}
-
-			// TODO (cthompson) will there be more than just one value in Data? currently we are only sending one chunk at a time.
-			embedding := res.Data[0].Embedding
-			embeddingData, err := json.Marshal(embedding)
-			if err != nil {
-				log.Error().
-					Err(err).
-					Str("url", ref.URL).
-					Msg("failed to marshal embedding data")
-				return err
-			}
-
-			newRefEmb := model.ReferenceEmbedding{
-				ReferenceContentID: ref.ReferenceContent.ID,
-				ContentHash:        hashStr,
-				Content:            formattedChunk,
-				Embedding:          string(embeddingData),
-			}
-
-			insertStmt := re.INSERT(
-				re.ReferenceContentID, re.ContentHash, re.Content, re.Embedding,
-			).MODEL(newRefEmb)
-
-			_, err = insertStmt.Exec(p.deps.DB)
-			if err != nil {
-				log.Error().
-					Err(err).
-					Msg("failed to insert reference embedding")
-				return err
-			}
-
-			if insertWithPinecone {
-				pineconeVectors = append(pineconeVectors, newVulnRefVector(hashStr, embedding, ref.URL, ref.Vulnerability.SourceID))
-
-				// 20 is used in the Buff script, why is it 20?
-				if len(pineconeVectors) == 20 {
-					log.Info().
-						Str("url", ref.URL).
-						Msg("upserting embedding to pinecone")
-
-					err = p.deps.PineconeClient.Upsert(pineconeVectors)
-					if err != nil {
-						log.Error().
-							Err(err).
-							Msg("failed to upsert pinecone vectors")
-						return err
-					}
-					pineconeVectors = []*pineconefx.Vector{}
-				}
-			}
-		}
-
-		if insertWithPinecone {
-			// upsert the remaining vectors to pinecone
-			err = p.deps.PineconeClient.Upsert(pineconeVectors)
-			if err != nil {
-				log.Error().
-					Err(err).
-					Msg("failed to upsert pinecone vectors")
-				return err
-			}
+		expBackoff := backoff.NewExponentialBackOff()
+		err := backoff.Retry(func() error {
+			return p.generateEmbeddingForRef(&ref, insertWithPinecone)
+		}, expBackoff)
+		if err != nil {
+			log.Error().
+				Err(err).
+				Str("url", ref.URL).
+				Msg("failed to generate embedding for reference after backoff")
+			return err
 		}
 	}
 	return nil
